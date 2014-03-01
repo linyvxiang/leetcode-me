6a54b0cb1768b0ea41d2e0a709409cf6f387da68
yvxiang <linyvxiang@gmail.com>
2014-03-01 21:31:16 +0800

    use binary search directly

--- SearchinRotatedSortedArray.cpp
+++ SearchinRotatedSortedArray.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    int find_small_index(int A[], int n)
-    {
-        int i = 1;
-        for(; i <n; i++) {
-            if(A[i] < A[0])
-                return i;
-        }
-        return -1;
-    }
-    int binary_search(int A[], int start, int end, int target)
-    {
-        while(start <= end) {
-            int mid = (start + end) >> 1;
+   
+    int search(int A[], int n, int target) {
+        int first = 0, last = n;
+        while(first != last) {
+            int mid = (first + last) >> 1;
             if(A[mid] == target)
                 return mid;
-            if(A[mid] > target) {
-                end = mid - 1;
-            } else
-                start = mid + 1;
+            if(A[first] <= A[mid]) {
+                if(A[first] <= target && target < A[mid])
+                    last = mid;
+                else
+                    first = mid + 1;
+            } else {
+                if(A[mid] < target && target <= A[last - 1] )
+                    first = mid + 1;
+                else
+                    last = mid;
+            }
         }
         return -1;
-    }
-    int search(int A[], int n, int target) {
-        if(n == 0)
-            return -1;
-        int pos = find_small_index(A, n);
-        if(pos != -1) {//OK, some one after A[0] is smaller, so the first part is bigger than the second part
-            if(A[0] <= target && target <= A[pos - 1]) {//maybe in the first part, couldn't in the second part
-                return binary_search(A, 0, pos - 1, target);
-            } else if(A[pos] <= target && target <= A[n - 1]) {// maybe in the second part
-                return binary_search(A, pos, n - 1, target);
-            } else
-                return -1;
-        } else { // A[0] is the smallest
-            return binary_search(A, 0, n - 1, target);
-        }
     }
 };
