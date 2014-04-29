a69424d4b997e6e6402071a0b7a0486bb59acdba
yvxiang <linyvxiang@gmail.com>
2014-04-29 22:20:03 +0800

    remove the useless prev var

--- CombinationSum.cpp
+++ CombinationSum.cpp
@@ -8,11 +8,9 @@ public:
         }
         if(cur_sum > target)
             return ;
-        int prev = -1, i;
+        int i;
         for(i = cur_pos; i < num.size(); i++) {
-            if(prev == num[i]) continue;
             cur_res.push_back(num[i]);
-            prev = num[i];
             do_slove(res, cur_res, num, cur_sum + num[i], i, target);
             cur_res.pop_back();
         }
