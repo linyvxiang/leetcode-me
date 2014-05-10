3d0c984498a9ee6332fde81ac5c7744edb8a472e
yvxiang <linyvxiang@gmail.com>
2014-05-10 19:11:57 +0800

    rewrite

--- TrappingRainWater.cpp
+++ TrappingRainWater.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int trap(int A[], int n) {
- 	    int *max_left = new int[n]();
- 	    int *max_right = new int[n]();
- 	    
- 	    int i;
- 	    for(i = 1; i < n; i++) {
- 	        max_left[i] = max(max_left[i - 1], A[i - 1]);
- 	        max_right[n - i - 1] = max(max_right[n - i], A[n - i]);
- 	    }
- 	    int sum = 0;
- 	    for(i = 0; i < n; i++) {
- 	        int height = min(max_left[i], max_right[i]);
- 	        if(height > A[i])
- 	            sum += height - A[i];
- 	    }
- 	    delete[] max_left;
- 	    delete[] max_right;
- 	    return sum;
+        int *right_max = new int[n]();
+        int *left_max = new int[n]();
+        
+        int i;
+        for(i = 1; i < n; i++) {
+            right_max[n - 1 - i] = max(right_max[n - i], A[n - i]);
+            left_max[i] = max(left_max[i - 1], A[i - 1]);
+        }
+        int sum = 0;
+        for(i = 0; i < n; i++) {
+            int m = min(right_max[i], left_max[i]);
+            if(m > A[i])
+                sum += m - A[i];
+        }
+        delete[] right_max;
+        delete[] left_max;
+        return sum;
     }
 };
