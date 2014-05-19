d5684e43819ee93137c9035cd65fff0a1d586bf6
yvxiang <linyvxiang@gmail.com>
2014-05-19 10:38:24 +0800

    mirrors方式中序遍历二叉树，不用栈，O(n)时间

--- RecoverBinarySearchTree.cpp
+++ RecoverBinarySearchTree.cpp
@@ -7,36 +7,60 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    void in_order_traversal(TreeNode *root, TreeNode **prev, TreeNode **first,
-                                                            TreeNode **last)
+    void check(TreeNode *prev, TreeNode *cur, TreeNode **first, TreeNode **second)
     {
-        if(!root)
-            return ;
-        if(root->left)
-            in_order_traversal(root->left, prev, first, last);
-        if(*prev && root->val < (*prev)->val) {
+        if(prev->val > cur->val) {
             if(!(*first)) {
-                *first = *prev;
-                *last = root;
+                *first = prev;
+                *second = cur;
+            } else {
+                *second = cur;
             }
-            else
-                *last = root;
         }
-        *prev = root;
-        if(root->right)
-            in_order_traversal(root->right, prev, first, last);
-
     }
     void recoverTree(TreeNode *root) {
         if(!root)
             return ;
-        TreeNode *prev = NULL, *first = NULL, *last = NULL;
-
-        in_order_traversal(root, &prev, &first, &last);
-        swap(first->val, last->val);
-
-        return ;
+        TreeNode *cur = root;
+        TreeNode *p = NULL;
+        TreeNode *first = NULL, *second = NULL;
+        while(cur) {
+            if(!cur->left) {
+                if(!p)
+                    p = cur;
+                else
+                    check(p, cur, &first, &second);
+                p = cur;
+                cur = cur->right;
+            } else {
+                TreeNode *prev = cur->left;
+                while(prev->right && prev->right != cur)
+                    prev = prev->right;
+                if(!prev->right) {
+                    prev->right = cur;
+                    cur = cur->left;
+                } else {
+                    prev->right = NULL;
+                    if(!p)
+                        p = cur;
+                    else
+                        check(p ,cur, &first, &second);
+                    p = cur;
+                    cur = cur->right;
+                }
+            }
+        }
+        swap(first->val, second->val);
     }
 };
