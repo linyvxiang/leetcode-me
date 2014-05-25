3f90ee42f7acbbe46e06d17637b69778d6a77d3f
yvxiang <linyvxiang@gmail.com>
2014-05-25 15:34:54 +0800

    easy task

--- PathSum.cpp
+++ PathSum.cpp
@@ -9,28 +9,22 @@
  */
 class Solution {
 public:
-    bool find_path(TreeNode *root, int cur_sum, int target_sum) {
-        cur_sum += root->val;
-        if(!root->left && !root->right && cur_sum == target_sum)
-            return true;
-        bool left = false, right = false;
-        if(root->left) {
-            left = find_path(root->left, cur_sum, target_sum);
-        }
-        if(left)
-            return true;
-        if(root->right) {
-            right = find_path(root->right, cur_sum, target_sum);
-        }
-        if(right)
-            return true;
-        return false;
-    }
     bool hasPathSum(TreeNode *root, int sum) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
         if(!root)
             return false;
-        return find_path(root, 0, sum);
+        sum -= root->val;
+        if(root->left == NULL && root->right == NULL && sum == 0)
+            return true;
+        if(root->left) {
+            bool l = hasPathSum(root->left, sum);
+            if(l)
+                return true;
+        }
+        if(root->right) {
+            bool r = hasPathSum(root->right, sum);
+            if(r)
+                return true;
+        }
+        return false;
     }
 };
