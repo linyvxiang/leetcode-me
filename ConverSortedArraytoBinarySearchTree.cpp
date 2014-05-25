2798a13d72f099b6c014ff3d524c1467a1d6543b
yvxiang <linyvxiang@gmail.com>
2014-05-25 09:30:49 +0800

    注意在传递参数vector<int> num时，不要使用值传递，否则会超内存。使用引用 传递，减少不必要的复制

--- ConverSortedArraytoBinarySearchTree.cpp
+++ ConverSortedArraytoBinarySearchTree.cpp
@@ -9,24 +9,22 @@
  */
 class Solution {
 public:
-    void create_tree(int start_pos, int end_pos, TreeNode* &root, vector<int> &num) {
-        if(end_pos < 0 || start_pos > end_pos)
-            return;
-        root = (TreeNode *)malloc(sizeof(TreeNode));
-        int mid = (start_pos + end_pos) >> 1;
-        root->val = num[mid];
-        root->left = NULL;
-        root->right = NULL;
-        create_tree(start_pos, mid - 1, root->left, num);
-        create_tree(mid + 1, end_pos, root->right, num);
+    void create_bst_tree(int start, int end, vector<int> &num, TreeNode **root)
+    {
+        if(start > end)
+            return ;
+        *root = (TreeNode *)malloc(sizeof(struct TreeNode));
+        memset(*root, 0, sizeof(struct TreeNode));
+        int mid = (start + end) >> 1;
+        (*root)->val = num[mid];
+        create_bst_tree(start, mid - 1, num, &((*root)->left));
+        create_bst_tree(mid + 1, end, num, &((*root)->right));
     }
     TreeNode *sortedArrayToBST(vector<int> &num) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
         if(num.size() == 0)
             return NULL;
         TreeNode *root = NULL;
-        create_tree(0, num.size() - 1, root, num);
+        create_bst_tree(0, num.size() - 1, num, &root);
         return root;
     }
 };
