5556163de02149455a86f56dd71ead5cf6edf5cf
yvxiang <linyvxiang@gmail.com>
2014-05-25 15:51:03 +0800

    easy task

--- PathSumII.cpp
+++ PathSumII.cpp
@@ -9,29 +9,30 @@
  */
 class Solution {
 public:
-    void find_path(TreeNode *root, int cur_sum, int target_sum, vector<vector<int>> &final_res, vector<int> &cur_res) {
-        cur_res.push_back(root->val);
-        cur_sum += root->val;
-        if(!root->left && !root->right && cur_sum == target_sum) {
-            final_res.push_back(cur_res);
+    void do_judge(vector<vector<int>> &res, vector<int> &tmp_res, TreeNode *root, int left_sum)
+    {
+        left_sum -= root->val;
+        tmp_res.push_back(root->val);
+        if(root->left == NULL && root->right == NULL) {
+            if(left_sum == 0) {
+                res.push_back(tmp_res);
+            }
         }
-        if(root->left) {
-            find_path(root->left, cur_sum, target_sum, final_res, cur_res);
-        }
-        if(root->right) {
-            find_path(root->right, cur_sum, target_sum, final_res, cur_res);
-        }
-        cur_res.pop_back();
+        
+        if(root->left)
+            do_judge(res, tmp_res, root->left, left_sum);
+        if(root->right)
+            do_judge(res, tmp_res, root->right, left_sum);
+            
+        tmp_res.pop_back();
         
     }
     vector<vector<int> > pathSum(TreeNode *root, int sum) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        vector<vector<int>> V;
+        vector<vector<int>> res;
         if(!root)
-            return V;
-        vector<int> temp_res;
-        find_path(root, 0, sum, V, temp_res);
-        return move(V);
+            return res;
+        vector<int> tmp_res;
+        do_judge(res, tmp_res, root, sum);
+        return move(res);
     }
 };
