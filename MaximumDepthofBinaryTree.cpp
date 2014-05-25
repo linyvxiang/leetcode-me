d47019d3a1b25a60c0ca097a65be23b29eaf0bce
yvxiang <linyvxiang@gmail.com>
2014-05-25 15:02:19 +0800

    easy task

--- MaximumDepthofBinaryTree.cpp
+++ MaximumDepthofBinaryTree.cpp
@@ -10,17 +10,8 @@
 class Solution {
 public:
     int maxDepth(TreeNode *root) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         if(!root)
             return 0;
-        if(!root->left && !root->right)
-            return 1;
-        int left_max = INT_MIN;
-        int right_max = INT_MIN;
-        if(root->left)
-            left_max = maxDepth(root->left);
-        if(root->right)
-            right_max = maxDepth(root->right);
-        return max(left_max, right_max) + 1;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
