a3f5da42e4495b77313d60ab8c905909b292579a
yvxiang <linyvxiang@gmail.com>
2014-06-08 21:32:30 +0800

    start from the begin and the end

--- SortColors.cpp
+++ SortColors.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     void sortColors(int A[], int n) {
-        int count[3] = {0};
-        int i;
-        for(i = 0; i < n; i++)
-            count[A[i]]++;
-        for(i = 0; i < n; i++) {
-            if(count[0]) {
-                A[i] = 0;
-                count[0]--;
-            } else if(count[1]) {
-                A[i] = 1;
-                count[1]--;
+        int slow = -1, fast = n, mid = 0;
+        while(mid < n) {
+            if(A[mid] == 0 && mid > slow) {
+                A[mid] = A[slow + 1];
+                A[slow + 1] = 0;
+                slow++;
+            } else if(A[mid] == 2 && mid < fast) {
+                A[mid] = A[fast - 1];
+                A[fast - 1] = 2;
+                fast--;
             } else {
-                A[i] = 2;
-                count[2]--;
+                mid++;
             }
         }
     }
