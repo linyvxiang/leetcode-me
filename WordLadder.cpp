eb63fd9c884fd5f3c93fcc667cf0ce376338ba65
yvxiang <linyvxiang@gmail.com>
2014-06-18 09:28:08 +0800

    TLE

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
-	typedef string state_t;
-	int ladderLength(string start, string end,
-		const unordered_set<string> &dict) {
-		if (start.size() != end.size()) return 0;
-		if (start.empty() || end.empty()) return 0;
-		queue<string> next, current; // 当前层，下一层
-		unordered_set<string> visited; // 判重
-		unordered_map<string, string > father;
-		int level = 0; // 层次
-		bool found = false;
-		current.push(start);
-		while (!current.empty() && !found) {
-			++level;
-			while (!current.empty() && !found) {
-				const string str(current.front()); current.pop();
-				for (size_t i = 0; i < str.size(); ++i) {
-					string new_word(str);
-					for (char c = 'a'; c <= 'z'; c++) {
-						if (c == new_word[i]) continue;
-						swap(c, new_word[i]);
-						if (new_word == end) {
-							found = true; //找到了
-							father[new_word] = str;
-							break;
-						}
-						if (dict.count(new_word) > 0
-							&& !visited.count(new_word)) {
-							next.push(new_word);
-							visited.insert(new_word);
-							father[new_word] = str;
-						}
-						swap(c, new_word[i]); // 恢复该单词
-					}
-				}
-			}
-			swap(next, current); //!!! 交换两个队列
-		}
-		if (found) return level+1;
-		else return 0;
-	}
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        int cur_steps = 0, cur_num = 1, res = -1, next_num = 0;
+        queue<string> Q;
+        unordered_set<string>::iterator it;
+        unordered_set<string> visited;
+        Q.push(start);
+        while(!Q.empty()) {
+            string tmp = Q.front();
+            Q.pop();
+            if(judge_string(tmp, end)) {
+                res = (cur_num == 0 ? cur_steps + 1 : cur_steps) + 2;
+                break;
+            }
+            for(it = dict.begin(); it != dict.end(); it++) {
+                if(visited.find(*it) == dict.end() && judge_string(tmp, (string)(*it))) {
+                    Q.push(*it);
+                    visited.insert(*it);
+                    next_num++;
+                }
+            }
+            cur_num--;
+            if(cur_num == 0) {
+                cur_num = next_num;
+                next_num = 0;
+                cur_steps++;
+            }
+
+        }
+        return res;
+    }
+private:
+    int judge_string(string &src, string dst)
+    {
+        int i, count = 0;
+        for(i = 0; i < src.size(); i++) {
+            if(src[i] != dst[i]) {
+                count++;
+                if(count > 1)
+                    break;
+            }
+        }
+        return count == 1;
+    }
 };
