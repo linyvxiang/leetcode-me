10fc2896535f6a18c531d71d9e40d3818227fb44
yvxiang <linyvxiang@gmail.com>
2014-06-27 09:27:41 +0800

    binary search

--- Sqrtx.cpp
+++ Sqrtx.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int sqrt(int x) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        int low = 0, high = x / 2 + 1;
-        long long mid, cur_x;
-        while(low <= high) {
-            mid = (low + high) / 2;
-            cur_x = mid * mid;
-            if(cur_x == x)
+        int left = 1, right = x / 2;
+        int mid, last_mid;
+        if(x < 2)
+            return x;
+            
+        while(left <= right) {
+            mid = (left + right) >> 1;
+            if(x / mid > mid) {
+                left = mid + 1;
+                last_mid = mid;
+            } else if(x / mid < mid) {
+                right = mid - 1;
+            } else {
                 return mid;
-            if(cur_x > x)
-                high = mid - 1;
-            else
-                low = mid + 1;
+            }
         }
-        return high;
+        return last_mid;
     }
 };
