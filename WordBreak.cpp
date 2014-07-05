d049323215326fc670323f26e94a28fbac0f26d9
yvxiang <linyvxiang@gmail.com>
2014-07-05 16:30:02 +0800

    不会用unordered_set，这个是网上的，但是思路跟我写的vector版本是相同的

--- WordBreak.cpp
+++ WordBreak.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string> &dict) {
-        bool res_table[s.size()];
-        int i;
-        res_table[0] = false;
-        for(i = 0; i < s.size(); i++) {
-            int j = 0;
-            res_table[i] = false;
-            for(j = 0; j < dict.size(); j++) {
-                if(check(res_table, i, s, dict[j])) {
-                    res_table[i] = true;
+    bool wordBreak(string s, unordered_set<string> &dict) {
+
+        vector<bool> f(s.size() + 1, false);
+        f[0] = true; 
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (f[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    f[i] = true;
                     break;
                 }
             }
         }
-        for(i = 0; i < s.size(); i++)
-            printf("%d\n", res_table[i]);
-        return res_table[s.size() - 1];
-    }
-private:
-    bool check(bool table[], int cur_pos, const string &s, const string &tmp)
-    {
-        int t = cur_pos - tmp.size();
-        if(t < -1)
-            return false;
-        if(t > -1 && table[cur_pos - tmp.size()] == false)
-            return false;
-        int i;
-        for(i = 0; i < tmp.size(); i++)
-            if(s[cur_pos - tmp.size() + 1 + i] != tmp[i])
-                return false;
-        return true;
+        return f[s.size()];
     }
 };
