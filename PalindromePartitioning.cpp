6158af8a571b506ccd5f21802c3eaa8b39b7debf
yvxiang <linyvxiang@gmail.com>
2014-07-06 18:21:06 +0800

    还有一个循环版的，不过没办法去结果里的重，主要思想是每加入一个新的字母时，都看看能不能和前面的已有划分中的连续个组成新的，但是结果里有重复元素，没办法去掉

--- PalindromePartitioning.cpp
+++ PalindromePartitioning.cpp
@@ -2,34 +2,45 @@ class Solution {
 public:
     vector<vector<string> > partition(string s) {
         vector<vector<string> > res;
-        vector<string> cur;
-        string str = "";
-        do_search(0, str, s, cur, res);
-        return res;
+        vector<vector<string> > prev;
+        vector<string> tp;
+        string ss;
+        ss += s[0];
+        tp.push_back(ss);
+        prev.push_back(tp);
+        int i, j, k, start;
+        for(i = 1; i < s.size(); i++) {
+            for(j = 0; j < prev.size(); j++) {
+                for(k = 0; k < prev[j].size(); k++) {
+                    string t = "";
+                    for(start = k; start < prev[j].size(); start++) {
+                        t += prev[j][start];
+                    }
+                    t += s[i];
+                    int m;
+                    vector<string> next;
+                    if(check(t)) {
+                        for(m = 0; m < k; m++) {
+                            next.push_back(prev[j][m]);
+                        }
+                        next.push_back(t);
+                        res.push_back(next);
+                    }
+            }
+                vector<string> tt = prev[j];
+                string ttt = "";
+                ttt += s[i];
+                tt.push_back(ttt);
+                res.push_back(tt);
+            }
+       
+            prev = res;
+            res.clear();
+        }
+        return prev;
     }
 private:
-    void do_search(int cur_pos, string cur_str, string &s, vector<string> &cur_res,
-                                vector<vector<string> > &res)
-    {
-        if(cur_pos > s.size())
-            return ;
-        if(cur_pos == s.size()) {
-            if(!cur_res.empty()) {
-                res.push_back(cur_res);
-            }
-            return ;
-        }
-        int start = cur_pos;
-        for(; start < s.size(); start++) {
-            cur_str += s[start];
-            if(check(cur_str)) {
-                cur_res.push_back(cur_str);
-                do_search(start + 1, "", s, cur_res, res);
-                cur_res.pop_back();
-            }
-        }
-    }
-    bool check(string &s)
+    bool check(string s)
     {
         if(s.size() == 1)
             return true;
