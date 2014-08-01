a413748556aa5b5fe2166ab982dee7fabc779573
yvxiang <linyvxiang@gmail.com>
2014-08-01 15:42:33 +0800

    网上能A的答案

--- MaxPointsonaLine.cpp
+++ MaxPointsonaLine.cpp
@@ -9,37 +9,28 @@
  */
 class Solution {
 public:
-    int maxPoints(vector<Point> &points) {
-        if(points.size() == 0 || points.size() == 1)
-            return points.size();
-        int cur_pos, sec_pos, front_pos, max_num = -1;
-        for(cur_pos = 0; cur_pos < points.size(); cur_pos++) {
-            for(sec_pos = cur_pos + 1; cur_pos < points.size(); cur_pos++) {
-                if(same_points(cur_pos, sec_pos, points))
-                    continue;
-                int diff_x = points[cur_pos].x - points[sec_pos].x;
-                int diff_y = points[cur_pos].y - points[cur_pos].y;
-                int cur_num = 2;
-                for(front_pos = 0; front_pos < points.size();
-                            front_pos++) {
-                    if(same_points(cur_pos, front_pos, points)
-                        || same_points(sec_pos, front_pos, points))
-                        continue;
-                    int dx = points[front_pos].x - points[cur_pos].x;
-                    int dy = points[front_pos].y - points[cur_pos].y;
-                    if(dx * diff_y == dy * diff_x)
-                        cur_num++;
-                }
-                if(cur_num > max_num)
-                    max_num = cur_num;
-            }
-        }
-        return max_num;
-    }
-private:
-    bool same_points(int a, int b, vector<Point> &points)
-    {
-        return points[a].x == points[b].x &&
-                points[a].y == points[b].y;
-    }
+	int maxPoints(vector<Point> &points) {
+		if (points.size() < 3) return points.size();
+		int result = 0;
+		for (int i = 0; i < points.size() - 1; i++) {
+			for (int j = i + 1; j < points.size(); j++) {
+				int sign = 0;
+				int a, b, c;
+				if (points[i].x == points[j].x) sign = 1;
+				else {
+					a = points[j].x - points[i].x;
+					b = points[j].y - points[i].y;
+					c = a * points[i].y - b * points[i].x;
+				}
+				int count = 0;
+				for (int k = 0; k < points.size(); k++) {
+					if ((0 == sign && a * points[k].y == c + b * points[k].x) ||
+						(1 == sign&&points[k].x == points[j].x))
+						count++;
+				}
+				if (count > result) result = count;
+			}
+		}
+		return result;
+	}
 };
