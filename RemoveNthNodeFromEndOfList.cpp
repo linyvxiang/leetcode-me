421f3229476f2bae79cc1f55bbb862ac343016c9
yvxiang <linyvxiang@gmail.com>
2014-08-03 10:52:04 +0800

    keep a clear mind

--- RemoveNthNodeFromEndOfList.cpp
+++ RemoveNthNodeFromEndOfList.cpp
@@ -9,36 +9,23 @@
 class Solution {
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        ListNode *front = head->next;
-        ListNode *bak = head;
-        while(n-- && front) {
-            front = front->next;
-        }
-        if(!front) {
-            if(!bak->next) {
-                free(bak);
-                return NULL;
-            }
-            if(bak == head && !n) {
-                ListNode *ans = head->next;
-                free(head);
-                return ans;
-            }
-            free(bak->next);
-            bak->next = bak->next->next;
+        if(!head)
             return head;
+        ListNode HEAD(0);
+        HEAD.next = head;
+        ListNode *fast = &HEAD, *slow = &HEAD, *d;
+        
+        while(n--) 
+            fast = fast->next;
+            
+        while(fast->next) {
+            fast = fast->next;
+            slow = slow->next;
         }
-        while(front) {
-            front = front->next;
-            bak = bak->next;
-        }
-        if(bak->next) {
-           ListNode *del = bak->next;
-           bak->next = del->next;
-           free(del);
-        }
-        return head;
+        
+        d = slow->next;
+        slow->next = d->next;
+        free(d);
+        return HEAD.next;
     }
 };
