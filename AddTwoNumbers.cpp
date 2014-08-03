86c715ef1e14d7e9d69aca7edb9582733e563bc5
yvxiang <linyvxiang@gmail.com>
2014-08-03 11:36:21 +0800

    easy task

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -8,55 +8,43 @@
  */
 class Solution {
 public:
-   
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        ListNode *head1 = (ListNode *)malloc(sizeof(ListNode));
-        ListNode *head2 = (ListNode *)malloc(sizeof(ListNode));
-        head1->next = l1;
-        head2->next = l2;
-    
-        ListNode *point1 = l1;
-        ListNode *point2 = l2;
-        ListNode *res = (ListNode *)malloc(sizeof(ListNode));
-        res->next = NULL;
-        ListNode *tmp = NULL;
-        ListNode * cur = res;
-        int flag = 0;
-        while(point1 && point2) {
-            tmp = (ListNode*)malloc(sizeof(ListNode));
-            tmp->next = NULL;
-            tmp->val = (point1->val + point2->val + flag) % 10;
-            flag = (point1->val + point2->val + flag) / 10;
-            cur->next = tmp;
-            cur = cur->next;
-            point1 = point1->next;
-            point2 = point2->next;
+        ListNode *res_head = NULL, *res_end;
+        int c = 0;
+        while(l1 && l2) {
+            if(!res_head) {
+                res_head = (ListNode *)malloc(sizeof(struct ListNode));
+                res_head->next = NULL;
+                res_head->val = (l1->val + l2->val + c) % 10;
+                c = (l1->val + l2->val + c) / 10;
+                res_end = res_head;
+            } else {
+                res_end->next = (ListNode *)malloc(sizeof(struct ListNode));
+                res_end = res_end->next;
+                res_end->next = NULL;
+                res_end->val = (l1->val + l2->val + c) % 10;
+                c = (l1->val + l2->val + c) / 10;
+            }
+            l1 = l1->next;
+            l2 = l2->next;
         }
-        while(point1) {
-            tmp = (ListNode *)malloc(sizeof(ListNode));
-            tmp->next = NULL;
-            tmp->val = (point1->val + flag) % 10;
-            flag = (point1->val + flag) / 10;
-            cur->next = tmp;
-            cur = cur->next;
-            point1= point1->next;
+
+        ListNode *l;
+        l = l1 ? l1 : l2;
+        while(l) {
+            res_end->next = (ListNode *)malloc(sizeof(struct ListNode));
+            res_end = res_end->next;
+            res_end->next = NULL;
+            res_end->val = (l->val + c) % 10;
+            c = (l->val + c) / 10;
+            l = l->next;
         }
-        while(point2) {
-            tmp = (ListNode *)malloc(sizeof(ListNode));
-            tmp->next = NULL;
-            tmp->val = (point2->val + flag) % 10;
-            flag = (point2->val + flag) / 10;
-            cur->next = tmp;
-            cur = cur->next;
-            point2 = point2->next;
+        if(c) {
+              res_end->next = (ListNode *)malloc(sizeof(struct ListNode));
+               res_end->next->val = 1;
+               res_end->next->next = NULL;
         }
-        if(flag) {
-            tmp = (ListNode *)malloc(sizeof(ListNode));
-            tmp->next = NULL;
-            tmp->val = flag;
-            cur->next = tmp;
-        }
-        
-        return res->next;
+
+        return res_head;
     }
 };
