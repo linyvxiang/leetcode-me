4e3ec912d1be8845b443a3e239832531b499dab8
yvxiang <linyvxiang@gmail.com>
2014-08-03 11:07:32 +0800

    keep a clear mind

--- SwapNodesInPairs.cpp
+++ SwapNodesInPairs.cpp
@@ -9,26 +9,19 @@
 class Solution {
 public:
     ListNode *swapPairs(ListNode *head) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        if(!head)
-            return NULL;
-        ListNode *root =(ListNode *)malloc(sizeof(ListNode));
-        root->next = head;
-        ListNode *last = root, *back = NULL, *front = NULL, *tmp = NULL;
-        while(last) {
-            if(!last->next)
-                break;
-            back = last->next;
-            if(!back->next)
-                break;
-            front = back->next;
-            tmp = front;
-            back->next = front->next;
-            last->next = tmp;
-            tmp->next = back;
-            last = back;
+        ListNode HEAD(0);
+        HEAD.next = head;
+        ListNode *pre = &HEAD, *first, *second, *end;
+        while(pre && pre->next && pre->next->next) {
+            first = pre->next;
+            second = first->next;
+            end = second->next;
+
+            pre->next = second;
+            first->next = end;
+            second->next = first;
+            pre = first;
         }
-        return root->next;
+        return HEAD.next;
     }
 };
