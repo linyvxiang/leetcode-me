b4c0f8ad712f4cf8f18c508bc60372b025491fe0
yvxiang <linyvxiang@gmail.com>
2014-08-04 15:14:21 +0800

    easy task

--- ReorderList.cpp
+++ ReorderList.cpp
@@ -11,30 +11,58 @@ public:
     void reorderList(ListNode *head) {
         if(!head || !head->next)
             return ;
-        
-        ListNode *cur = head;
-        while(cur) {
-           ListNode *tail = find_tail(cur);
-           if(!tail || tail == cur)
-               break;
-           tail->next = cur->next;
-           cur->next = tail;
-           cur = tail->next;
-        }
+        int len = get_len(head);
+        ListNode *front_end = find_mid(head, len / 2 + 1);
+        ListNode *reverse_head = front_end->next;
+        front_end->next = NULL;
+        reverse_head = reverse_list(reverse_head);
+        insert_list(head, reverse_head);
     }
 private:
-    ListNode *find_tail(ListNode *start)
+    int get_len(ListNode *head)
     {
-      ListNode *cur = start;
-      ListNode *pre = NULL, *ppre = NULL;
-      while(cur) {
-          ppre = pre;
-          pre = cur;
-          cur = cur->next;
-      }
-      // pre is the tail;
-      if(ppre)
-          ppre->next = NULL;
-      return pre;
+        int l = 0;
+        ListNode *cur = head;
+        while(cur) {
+            l++;
+            cur = cur->next;
+        }
+        return l;
+    }
+    ListNode *find_mid(ListNode *head, int target_pos)
+    {
+        int cur_pos = 0;
+        ListNode HEAD(0);
+        HEAD.next = head;
+        ListNode *cur = &HEAD;
+        while(cur_pos < target_pos) {
+            cur_pos++;
+            cur = cur->next;
+        }
+        return cur;
+    }
+    ListNode *reverse_list(ListNode *head)
+    {
+        ListNode HEAD(0);
+        HEAD.next = NULL;
+        ListNode *cur = head;
+        while(cur) {
+            ListNode *tmp = cur->next;
+            cur->next = HEAD.next;
+            HEAD.next = cur;
+            cur = tmp;
+        }
+        return HEAD.next;
+    }
+    void insert_list(ListNode *left, ListNode *right)
+    {
+        ListNode *left_cur = left, *right_cur = right;
+        while(right_cur) {
+            ListNode *tmp = right_cur->next;
+            right_cur->next = left_cur->next;
+            left_cur->next = right_cur;
+            left_cur = left_cur->next->next;
+            right_cur = tmp;
+        }
     }
 };
