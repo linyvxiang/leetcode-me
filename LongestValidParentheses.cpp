5ee4b594224ede3b432d34fd83021e23420894d5
yvxiang <linyvxiang@gmail.com>
2014-08-06 14:25:53 +0800

    先扫描一遍，记住每个括号所在的位置是不是被匹配，然后再扫一遍，看看最长的连续的被匹配标记

--- LongestValidParentheses.cpp
+++ LongestValidParentheses.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> left_pos;
-        int last = -1, pos, max_len = 0;
+        bool matched[s.size()];
+        int pos;
+        memset(matched, false, sizeof(matched));
+            
+        stack<int> S;
         for(pos = 0; pos < s.size(); pos++) {
             if(s[pos] == '(') {
-                left_pos.push(pos);
+                S.push(pos);
             } else {
-                if(left_pos.empty()) {
-                    last = pos;
-                } else {
-                    left_pos.pop();
-                    if(left_pos.empty())
-                        max_len = max(max_len, pos - last);
-                    else 
-                        max_len = max(max_len, pos - left_pos.top());
+                if(!S.empty()) {
+                    matched[S.top()] = true;
+                    matched[pos] = true;
+                    S.pop();
                 }
             }
+        }
+
+        int max_len = 0, cur_len = 0;
+        for(pos = 0; pos < s.size(); pos++) {
+            if(matched[pos])
+                cur_len++;
+            else
+                cur_len = 0;
+            max_len = max(max_len, cur_len);
         }
         return max_len;
     }
