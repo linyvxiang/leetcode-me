408de9dfa63971718b48c1df949b561b3b39a7ba
yvxiang <linyvxiang@gmail.com>
2014-08-06 11:30:08 +0800

    good problem

--- LongestValidParentheses.cpp
+++ LongestValidParentheses.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int i, last = -1;
-        stack<int> lefts;
-        int max_len = 0;
-        for(i = 0; i < s.size(); i++) {
-            if(s[i] == '(') {
-                lefts.push(i);
+        stack<int> left_pos;
+        int last = -1, pos, max_len = 0;
+        for(pos = 0; pos < s.size(); pos++) {
+            if(s[pos] == '(') {
+                left_pos.push(pos);
             } else {
-                if(lefts.empty())
-                    last = i;
-                else {
-                    lefts.pop();
-                    if(lefts.empty()) {
-                        max_len = max(max_len, i - last);
-                    } else {
-                        max_len = max(max_len, i - lefts.top());
-                    }
+                if(left_pos.empty()) {
+                    last = pos;
+                } else {
+                    left_pos.pop();
+                    if(left_pos.empty())
+                        max_len = max(max_len, pos - last);
+                    else 
+                        max_len = max(max_len, pos - left_pos.top());
                 }
             }
         }
