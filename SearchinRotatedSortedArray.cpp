5bd6a5a7bb056376b0b8dcaf646cff22db41f719
yvxiang <linyvxiang@gmail.com>
2014-08-07 10:00:58 +0800

    二分查找 二分查找的变形。对于low, mid, high，要么[low, mid]是有序的，要么[mid, high]是有序的，找出这两个区间中有序的那个，如果target在这个区间中,那么在这个区间中继续二分查找，如果不在，则在另一个区间中二分查找

--- SearchinRotatedSortedArray.cpp
+++ SearchinRotatedSortedArray.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    int find_small_index(int A[], int n)
-    {
-        int i = 1;
-        for(; i <n; i++) {
-            if(A[i] < A[0])
-                return i;
-        }
-        return -1;
-    }
-    int binary_search(int A[], int start, int end, int target)
-    {
-        while(start <= end) {
-            int mid = (start + end) >> 1;
+    int search(int A[], int n, int target) {
+        if(!n)
+            return -1;
+        int low = 0, high = n - 1, mid;
+        while(low <= high) {
+            mid = (low + high) >> 1;
             if(A[mid] == target)
                 return mid;
-            if(A[mid] > target) {
-                end = mid - 1;
-            } else
-                start = mid + 1;
+            if(A[low] < A[mid] && target < A[mid] && target >= A[low]) {
+                high = mid - 1;
+            } else if(A[low] > A[mid] && !(target > A[mid] && target <= A[high])) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
         return -1;
-    }
-    int search(int A[], int n, int target) {
-        if(n == 0)
-            return -1;
-        int pos = find_small_index(A, n);
-        if(pos != -1) {//OK, some one after A[0] is smaller, so the first part is bigger than the second part
-            if(A[0] <= target && target <= A[pos - 1]) {//maybe in the first part, couldn't in the second part
-                return binary_search(A, 0, pos - 1, target);
-            } else if(A[pos] <= target && target <= A[n - 1]) {// maybe in the second part
-                return binary_search(A, pos, n - 1, target);
-            } else
-                return -1;
-        } else { // A[0] is the smallest
-            return binary_search(A, 0, n - 1, target);
-        }
     }
 };
