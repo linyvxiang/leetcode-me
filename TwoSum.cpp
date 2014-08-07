8d1b0b635ee01fedc0682b81a47b8412beab8f68
yvxiang <linyvxiang@gmail.com>
2014-08-07 20:00:42 +0800

    hash

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,34 +1,19 @@
-bool lessthan(const pair<int, int>& lhs, const pair<int, int>& rhs) {
-    if (lhs.first == rhs.first) 
-        return lhs.second < rhs.second;
-    return lhs.first < rhs.first;
-}
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-    
-    int i,j;
-    vector<pair<int,int>> V;
-    for(i = 0; i < numbers.size(); i++) {
-        V.push_back(make_pair(numbers[i],i+1));
-    }
-    sort(V.begin(),V.end(),lessthan);
-    i = 0;
-    j = V.size() - 1;
-    vector<int> ans;
-    while(i < j) {
-        if(V[i].first + V[j].first == target) {
-            ans.push_back(min(V[i].second,V[j].second));
-            ans.push_back(max(V[i].second,V[j].second));
-            break;
-        } else if(V[i].first + V[j].first < target) {
-            i++;
-        } else {
-            j--;
+        unordered_map<int, int> hash_map;
+        vector<int> res;
+        int i;
+        for(i = 0; i < numbers.size(); i++)
+            hash_map[numbers[i]] = i;
+        for(i = 0; i < numbers.size(); i++) {
+            int diff = target - numbers[i];
+            if(hash_map.find(diff) != hash_map.end() && hash_map[diff] != i) {
+                res.push_back(min(i, hash_map[diff]) + 1);
+                res.push_back(max(i, hash_map[diff]) + 1);
+                break;
+            }
         }
+        return res;
     }
-    return ans;
-   }
 };
