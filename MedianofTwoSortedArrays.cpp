f7c350bbd82e9dbed5dce63848315ef56e5617c2
yvxiang <linyvxiang@gmail.com>
2014-08-07 15:17:56 +0800

    类似二分查找 好题，自己想不出来，详解见evernote

--- MedianofTwoSortedArrays.cpp
+++ MedianofTwoSortedArrays.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int find_kth(int A[], int m, int B[], int n, int kth)
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        if((m + n) & 0x1) { // odd
+            return do_find_mid(A, m, B, n, (m + n) / 2 + 1);
+        } else {
+            return (do_find_mid(A, m, B, n, (m + n) / 2) +
+                    do_find_mid(A, m, B, n, (m + n) / 2 + 1)) / 2.0;
+        }
+    }
+private:
+    int do_find_mid(int A[], int m, int B[], int n, int k)
     {
         if(m > n)
-            return find_kth(B, n, A, m, kth);
+            return do_find_mid(B, n, A, m, k);
         if(m == 0)
-            return B[kth - 1];
-        if(kth == 1)
+            return B[k - 1];
+        if(k == 1)
             return min(A[0], B[0]);
-        
-        int pa = min(kth / 2, m), pb = kth - pa;
-        if(A[pa - 1] < B[pb - 1])
-            return  find_kth(A + pa, m - pa, B, n, kth - pa);
-        else if(A[pa - 1] > B[pb - 1])
-            return find_kth(A, m, B + pb, n - pb, kth - pb);
-        else
-            return A[pa - 1];
-    }
-    double findMedianSortedArrays(int A[], int m, int B[], int n) {
-        int N = (m + n);
-        if(N & 0x1)// odd
-            return find_kth(A, m, B, n, N / 2 + 1);
-        else
-            return (find_kth(A, m, B, n, N / 2) + find_kth(A, m, B, n, N / 2 + 1)) * 0.5;
+
+        int m1 = min(m, k / 2), m2 = k - m1;
+        if(A[m1 - 1] == B[m2 - 1]) {
+            return A[m1 - 1];
+        } else if(A[m1 - 1] < B[m2 - 1]) {
+            return do_find_mid(A + m1, m - m1, B, n, k - m1);
+        } else {
+            return do_find_mid(A, m, B + m2, n - m2, k - m2);
+        }
     }
 };
