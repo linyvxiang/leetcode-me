ab49c96702d845b3c2d350863f7ea28fb9cdd780
yvxiang <linyvxiang@gmail.com>
2014-08-08 20:54:37 +0800

    easy task

--- JumpGame.cpp
+++ JumpGame.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool canJump(int A[], int n) {
-        int i;
-        int step = A[0];
         if(n == 1)
             return true;
-        
-        for(i = 0; i < n; i++) {
-            if(step > 0) {
-                step--;
-                step = max(step, A[i]);
-            } else {
+        int max_len = 0, cur_pos;
+        for(cur_pos = 0; cur_pos < n; cur_pos++) {
+            max_len = max(max_len, cur_pos + A[cur_pos]);
+            if(max_len == cur_pos)
                 return false;
-            }
+            if(max_len >= n - 1)
+                return true;
         }
-        return  true;
+        return false;
     }
 };
