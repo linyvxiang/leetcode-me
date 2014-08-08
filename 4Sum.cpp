ef350231a40c4978498b588a3995f84fbfc7f96c
yvxiang <linyvxiang@gmail.com>
2014-08-08 15:49:44 +0800

    hash法 先缓存两个数的和

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,58 +1,30 @@
 class Solution {
 public:
-	vector<vector<int>> find_3_sum(vector<int> num, int start, int end, int target)
-	{
-		int i = start;
-		vector<vector<int>> res;
-		while(i < end - 2){
-			int low = i + 1, high = end - 1;
-			while(low < high) {
-				int cur_sum = num[i] + num[low] + num[high];
-				if(cur_sum == target) {
-					vector<int> tmp;
-					tmp.push_back(num[i]); 
-					tmp.push_back(num[low]);
-					tmp.push_back(num[high]);
-					res.push_back(tmp);
-					low++;
-					while(low < high && num[low] == num[low - 1])
-						low++;
-					high--;
-					while(high > low && num[high] == num[high + 1])
-						high--;
-				} else if(cur_sum > target) {
-					high--;
-				} else {
-					low++;
-				}
-			}
-			i++;
-			while( i < end - 2 && num[i] == num[i - 1])
-				i++;
-		}
-		return move(res);
-	}
-	
-    vector<vector<int> > fourSum(vector<int> &num, int target) {
- 		vector<vector<int>> res;
-		int size = num.size();
-		if(num.empty() || size < 4)
-			return res;
-		int i = 0;
+	vector<vector<int>> fourSum(vector<int>& num, int target) {
+		vector<vector<int>> result;
+		if (num.size() < 4) return result;
 		sort(num.begin(), num.end());
-		while(i < size - 3) {
-			vector<vector<int>> tmp = find_3_sum(num, i + 1, size, target - num[i]);
-			if(!tmp.empty()) {
-				int j;
-				for(j = 0; j < tmp.size(); j++) {
-					tmp[j].insert(tmp[j].begin(), num[i]);
-					res.push_back(tmp[j]);
+		unordered_multimap<int, pair<int, int>> cache;
+		for (int i = 0; i + 1 < num.size(); ++i)
+		for (int j = i + 1; j < num.size(); ++j)
+			cache.insert(make_pair(num[i] + num[j], make_pair(i, j)));
+		for (auto i = cache.begin(); i != cache.end(); ++i) {
+			int x = target - i->first;
+			auto range = cache.equal_range(x);
+			for (auto j = range.first; j != range.second; ++j) {
+				auto a = i->second.first;
+				auto b = i->second.second;
+				auto c = j->second.first;
+				auto d = j->second.second;
+				if (a != c && a != d && b != c && b != d) {
+					vector<int> vec = { num[a], num[b], num[c], num[d] };
+					sort(vec.begin(), vec.end());
+					result.push_back(vec);
 				}
 			}
-			i++;
-			while(i < size - 3 && num[i] == num[i - 1])
-			    i++;
 		}
-		return move(res);
-    }
+		sort(result.begin(), result.end());
+		result.erase(unique(result.begin(), result.end()), result.end());
+		return result;
+	}
 };
