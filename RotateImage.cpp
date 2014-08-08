53cb59d6d9393d1c4b96ff78ee0d8d5952102797
yvxiang <linyvxiang@gmail.com>
2014-08-08 19:01:49 +0800

    easy task

--- RotateImage.cpp
+++ RotateImage.cpp
@@ -3,10 +3,10 @@ public:
     void rotate(vector<vector<int> > &matrix) {
         int i, j, n = matrix.size();
         for(i = 0; i < n; i++)
-            for(j = 0; j < n - i; j++)
-                swap(matrix[i][j], matrix[n - 1 - j][n -  1 - i]);
-        for(i = 0; i < n / 2; i++)
-            for(j = 0; j < n; j++)
-                swap(matrix[i][j], matrix[n - 1 - i][j]);
+            for(j = 0; j < i; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        for(i = 0; i < n; i++)
+            for(j = 0; j < n / 2; j++)
+                swap(matrix[i][j], matrix[i][n - j - 1]);
     }
 };
