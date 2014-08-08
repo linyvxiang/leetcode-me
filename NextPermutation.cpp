b1efbd857e735b071e1766569ee34cfe30398e66
yvxiang <linyvxiang@gmail.com>
2014-08-08 16:48:02 +0800

    详解见evernote

--- NextPermutation.cpp
+++ NextPermutation.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void nextPermutation(vector<int> &num) {
-        int l = num.size() - 2;
-        for(; l >= 0; l--) {
-            if(num[l] < num[l + 1]) { //find the left pos
-                int r;
-                for(r = num.size() - 1; r > l ; r--) {
-                    if(num[r] > num[l]) {
-                        swap(num[l], num[r]);
-                        reverse(num.begin() + l + 1, num.end());
+        int pn_pos = num.size() - 2, cn_pos;
+        for(; pn_pos >= 0; pn_pos--) {
+            if(num[pn_pos] < num[pn_pos + 1]) {
+                for(cn_pos = num.size() - 1; cn_pos >= 0; cn_pos--) {
+                    if(num[cn_pos] > num[pn_pos]) {
+                        swap(num[pn_pos], num[cn_pos]);
+                        reverse(num.begin() + pn_pos + 1, num.end());
                         return ;
                     }
                 }
