dd2daf25626b204fe2d13ef9420e18ad64895976
yvxiang <linyvxiang@gmail.com>
2014-08-10 16:50:36 +0800

    好题 其实是分三种情况讨论，1)被插入区间完全在当前区间左侧 这时直接插入就可以 2)被插入敬意完全在当前区间右边，这时有可能被插入区间可以和下一个区间合并，所以往 下走一步看  3)被插入区间与 当前区间重合，则整合出新的区间，再往下看

--- InsertInterval.cpp
+++ InsertInterval.cpp
@@ -8,27 +8,29 @@
  * };
  */
 class Solution {
-private:
-    static bool comp(Interval a, Interval b)
-    {
-        return a.start < b.start;
-    }
 public:
     vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-        //在原始数组上操作
-        vector<Interval>::iterator ite = lower_bound(intervals.begin(),intervals.end(), newInterval, comp);//按照start值二分查找
-        if(ite != intervals.begin() && newInterval.start <= (ite-1)->end)//ite的上一个区间也可能参与合并
-        {
-            ite--;
-            //合并后新区间的起点只和第一个合并的区间有关，因为数组时按区间起点有序的
-            newInterval.start = min(newInterval.start, ite->start);
+        vector<Interval>::iterator it = intervals.begin();
+        vector<Interval>::iterator del_start = intervals.begin();
+        while(it != intervals.end()) {
+            if(newInterval.end < it->start) {
+                if(del_start != it)
+                    it = intervals.erase(del_start, it);
+                intervals.insert(it, newInterval);
+                return intervals;
+            }
+            if(it->end < newInterval.start) {
+                del_start = it + 1;
+                it++;
+            } else {
+                newInterval.start = min(it->start, newInterval.start);
+                newInterval.end = max(it->end, newInterval.end);
+                it++;
+            }
         }
-        vector<Interval>::iterator eraseBegin = ite;
-        for(; ite != intervals.end() && newInterval.end >= ite->start; ite++)
-            if(newInterval.end < ite->end)newInterval.end = ite->end;//合并后的新区间存放于newInterval
-			
-			ite = intervals.erase(eraseBegin, ite);//[eraseBegin, ite)是合并时应该删掉的区间
-			intervals.insert(ite, newInterval);//插入合并后的区间
-			return intervals;
+        if(del_start != it)
+            intervals.erase(del_start, it);
+        intervals.insert(intervals.end(), newInterval);
+        return intervals;
     }
 };
