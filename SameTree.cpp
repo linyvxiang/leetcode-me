d979837be17dc602d809a1e7a6901f90911c0202
yvxiang <linyvxiang@gmail.com>
2014-08-13 16:31:38 +0800

    rewrite

--- SameTree.cpp
+++ SameTree.cpp
@@ -16,7 +16,7 @@ public:
             return false;
         if(p->val != q->val)
             return false;
-        return isSameTree(p->left, q->left) && 
+        return isSameTree(p->left, q->left) &&
                 isSameTree(p->right, q->right);
     }
 };
