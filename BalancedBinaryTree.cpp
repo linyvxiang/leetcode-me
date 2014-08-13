69fb6baa577bebbfc9eb6559ae798d9a016ae75b
yvxiang <linyvxiang@gmail.com>
2014-08-13 16:56:38 +0800

    注意效率 从顶往下的递归可能会导致每个节点被遍历多次。所以最好不要先求左右子树的高度再判断左右子树是不是平衡，而是从下面往上走，同时注意剪枝，当左子树不满足要求时立即停止。

--- BalancedBinaryTree.cpp
+++ BalancedBinaryTree.cpp
@@ -9,22 +9,20 @@
  */
 class Solution {
 public:
-    int calc_height(TreeNode *root)
+    bool isBalanced(TreeNode *root) {
+        return balanced_tree_height(root) >= 0;
+    }
+private:
+    int balanced_tree_height(TreeNode *root)
     {
         if(!root)
             return 0;
-        return max(calc_height(root->left), calc_height(root->right)) + 1;
-    }
-    bool isBalanced(TreeNode *root) {
-        if(!root)
-            return true;
-        if(isBalanced(root->left) && isBalanced(root->right)) {
-            int l_height = calc_height(root->left);
-            int r_height = calc_height(root->right);
-            int diff = (l_height > r_height) ? (l_height - r_height) : (r_height - l_height);
-            if(diff <= 1)
-                return true;
-        }
-        return false;
+        int left_height = balanced_tree_height(root->left);
+        if(left_height < 0)
+            return -1;
+        int right_height = balanced_tree_height(root->right);
+        if(right_height < 0 || abs(left_height - right_height) > 1)
+            return -1;
+        return max(left_height, right_height) + 1;
     }
 };
