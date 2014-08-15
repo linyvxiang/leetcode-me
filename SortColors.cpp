f3db129d5fe4bc8ae6077528ffc3a2a31044948d
yvxiang <linyvxiang@gmail.com>
2014-08-15 15:44:01 +0800

    rewrite

--- SortColors.cpp
+++ SortColors.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     void sortColors(int A[], int n) {
-        int slow = -1, fast = n, mid = 0;
-        while(mid < n) {
-            if(A[mid] == 0 && mid > slow) {
-                A[mid] = A[slow + 1];
-                A[slow + 1] = 0;
+        int slow = -1, fast = n, cur = 0;
+        while(cur < n) {
+            if(A[cur] == 0 && slow < cur) {
+                swap(A[cur], A[slow + 1]);
                 slow++;
-            } else if(A[mid] == 2 && mid < fast) {
-                A[mid] = A[fast - 1];
-                A[fast - 1] = 2;
+            } else if(A[cur] == 2 && cur < fast) {
+                swap(A[cur], A[fast - 1]);
                 fast--;
             } else {
-                mid++;
+                cur++;
             }
         }
     }
