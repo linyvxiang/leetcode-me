a3935e2d1a13af21461c5cc7b71d660e84a8ef87
yvxiang <linyvxiang@gmail.com>
2014-08-15 09:53:44 +0800

    小优化 最后不用判断A是否已经耗完，因为即使没耗完的话，A的剩余元素在原地不动也是正确的

--- MergeSortedArray.cpp
+++ MergeSortedArray.cpp
@@ -3,21 +3,9 @@ public:
     void merge(int A[], int m, int B[], int n) {
         int fin_pos = m + n - 1;
         int pos_a = m - 1, pos_b = n - 1;
-        while(pos_a >= 0 && pos_b >= 0) {
-            if(A[pos_a] > B[pos_b]) {
-                A[fin_pos--] = A[pos_a--];
-            } else {
+        while(pos_a >= 0 && pos_b >= 0)
+            A[fin_pos--] = A[pos_a] > B[pos_b] ? A[pos_a--] : B[pos_b--];
+        while(pos_b >= 0)
                 A[fin_pos--] = B[pos_b--];
-            }
-        }
-        if(pos_a >= 0) {
-            while(pos_a >= 0) {
-                A[fin_pos--] = A[pos_a--];
-            }
-        } else {
-            while(pos_b >= 0) {
-                A[fin_pos--] = B[pos_b--];
-            }
-        }
     }
 };
