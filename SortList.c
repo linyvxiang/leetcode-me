ee9ad39afe73f074a07bacaa6261e93247083ceb
yvxiang <linyvxiang@gmail.com>
2014-08-15 11:07:15 +0800

    rewrite

--- SortList.c
+++ SortList.c
@@ -8,45 +8,48 @@
  */
 class Solution {
 public:
-  ListNode* merge(ListNode* h1, ListNode* h2) {
-        ListNode* dump = new ListNode(0);
-        ListNode* p = dump;
-        while(h1!=NULL && h2!=NULL) {
-            if(h1->val<h2->val){
-                p->next = h1;
-                h1 = h1->next;
-            } else {
-                p->next = h2;
-                h2 = h2->next;
-            }
+    ListNode *sortList(ListNode *head) {
+        int len = 0;
+        ListNode *p = head;
+        while(p) {
+            len++;
             p = p->next;
         }
-        
-        if(h1!=NULL) p->next = h1;
-        else p->next = h2;
-        ListNode* head = dump->next;
-        delete dump;
-        return head;
-  }
-    ListNode *do_sort(ListNode *head)
-    {
-        if(!head || !head->next)
-            return head;
-        ListNode *right_start = head, *pre = head, *right_end = head;
-        while(right_end && right_end->next) {
-            right_end = right_end->next->next;
-            pre = right_start;
-            right_start = right_start->next;
-        }
-        pre->next = NULL;
-        ListNode *l = do_sort(head);
-        ListNode *r = do_sort(right_start);
-        merge(l, r);
+        return merge_sort_list(head, len);
     }
-    ListNode *sortList(ListNode *head) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        //merge sort
-        return do_sort(head);
+private:
+    ListNode *merge_sort_list(ListNode *head, int len)
+    {
+        if(len == 0)
+            return NULL;
+        if(len == 1)
+            return head;
+        int l = len / 2;
+        ListNode HEAD(0), *cur = &HEAD;
+        HEAD.next = head;
+        while(l--)
+            cur = cur->next;
+        ListNode *right = cur->next;
+        cur->next = NULL;
+        ListNode *left = merge_sort_list(HEAD.next, len / 2);
+        right = merge_sort_list(right, len - (len / 2));
+        return do_merge(left, right);
+    }
+    ListNode *do_merge(ListNode *left, ListNode *right)
+    {
+        ListNode HEAD(0), *tail = &HEAD;
+        while(left && right) {
+            if(left->val < right->val) {
+                tail->next = left;
+                left = left->next;
+            } else {
+                tail->next = right;
+                right = right->next;
+            }
+            tail = tail->next;
+            tail->next = NULL;
+        }
+        tail->next = left ? left : right;
+        return HEAD.next;
     }
 };
