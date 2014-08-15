952987989836b87f292d36456740e9792f51c118
yvxiang <linyvxiang@gmail.com>
2014-08-15 16:02:22 +0800

    a more readable version

--- SearchForARange.cpp
+++ SearchForARange.cpp
@@ -1,33 +1,56 @@
 class Solution {
 public:
     vector<int> searchRange(int A[], int n, int target) {
-        int left_bound = -1, right_bound = -1;
-        find_bound(A, 0, n - 1, target, left_bound, 0);
-        find_bound(A, 0, n - 1, target, right_bound, 1);
         vector<int> res;
-        if(left_bound > right_bound)
-            swap(left_bound, right_bound);
-        res.push_back(left_bound);
-        res.push_back(right_bound);
+        do_search(A, 0, n - 1, target, res);
+        if(res.empty()){
+            res.push_back(-1);
+            res.push_back(-1);
+        }
         return res;
     }
 private:
-    void find_bound(int A[], int start, int end, int target, int &bound, int direc)
+    void do_search(int A[], int start_pos, int end_pos, int target, vector<int> &res)
+    {
+        if(start_pos > end_pos)
+            return ;
+        int mid = (start_pos + end_pos) >> 1;
+        if(A[mid] == target) {
+            int left_bound = -1, right_bound;
+            find_left_bound(A, start_pos, mid, left_bound, target);
+            find_right_bound(A, mid, end_pos, right_bound, target);
+            res.push_back(left_bound);
+            res.push_back(right_bound);
+            return ;
+        } else if(A[mid] > target) {
+            do_search(A, start_pos, mid - 1, target, res);
+        } else {
+            do_search(A, mid + 1, end_pos, target, res);
+        }
+    }
+private:
+    void find_left_bound(int A[], int start, int end, int &res, int target)
     {
         if(start > end)
-            return;
+            return ;
         int mid = (start + end) >> 1;
         if(A[mid] == target) {
-            bound = mid;
-            if(direc == 0)
-                find_bound(A, mid + 1, end, target, bound, direc);
-            else
-                find_bound(A, start, mid - 1, target, bound, direc);
-        } else {
-            if(A[mid] > target)
-                find_bound(A, start, mid - 1, target, bound, direc);
-            else 
-                find_bound(A, mid + 1, end, target, bound, direc);
+            res = mid;
+            find_left_bound(A, start, mid - 1, res, target);
+        } else if(A[mid] < target) {
+            find_left_bound(A, mid + 1, end, res, target);
+        }
+    }
+    void find_right_bound(int A[], int start, int end, int &res, int target)
+    {
+        if(start > end)
+            return ;
+        int mid = (start + end) >> 1;
+        if(A[mid] == target) {
+            res = mid;
+            find_right_bound(A, mid + 1, end, res, target);
+        } else if(A[mid] > target) {
+            find_right_bound(A, start, mid - 1, res, target);
         }
     }
 };
