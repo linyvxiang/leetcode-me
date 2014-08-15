84d8aef3c5de07ab321dc953bb1407d040786a2a
yvxiang <linyvxiang@gmail.com>
2014-08-15 14:27:25 +0800

    rewrite

--- FirstMissingPositive.cpp
+++ FirstMissingPositive.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int firstMissingPositive(int A[], int n) {
-        int i = 0;
-        while(i < n) {
-            if(A[i] != i + 1 && A[i] >= 1 && A[i] <= n && A[A[i] - 1] != A[i])
-                swap(A[i], A[A[i] - 1]);
-            else
+        int i;
+        for(i = 0; i < n; ) {
+            if(A[i] == i)
+                i++;
+            else if(0 <= A[i] && A[i] < n && A[A[i]] != A[i])
+                swap(A[i], A[A[i]]);
+            else 
                 i++;
         }
-        for(i = 0; i < n; i++)
-            if(A[i] != i + 1)
-                return i + 1;
-        return n + 1;
+        for(i = 1; i < n; i++)
+            if(A[i] != i)
+                return i;
+        return A[0] == n ? n + 1 : n;
     }
 };
