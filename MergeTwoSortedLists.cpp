ae9c756922663064126e2cd147f7d0597243c507
yvxiang <linyvxiang@gmail.com>
2014-08-15 10:02:35 +0800

    rewrite

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -9,34 +9,18 @@
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        ListNode HEAD(0);
-        HEAD.next = NULL;
-        ListNode *cur_final = &HEAD, *cur_pos_l1 = l1, *cur_pos_l2 = l2;
+        ListNode HEAD(0), *tail = &HEAD;
         while(l1 && l2) {
-            if(l1->val < l2->val) {//move l1 to the right pos
-                cur_final->next = l1;
+            if(l1->val < l2->val) {
+                tail->next = l1;
                 l1 = l1->next;
             } else {
-                cur_final->next = l2;
+                tail->next = l2;
                 l2 = l2->next;
             }
-            cur_final = cur_final->next;
-            cur_final->next = NULL;
+            tail = tail->next;
         }
-        if(l1) {
-            while(l1) {
-                cur_final->next = l1;
-                cur_final = cur_final->next;
-                l1 = l1->next;
-            }
-        } else {
-            while(l2) {
-                cur_final->next = l2;
-                cur_final = cur_final->next;
-                l2 = l2->next;
-            }
-        }
-        cur_final->next = NULL;
+        tail->next = l1 ? l1 : l2;
         return HEAD.next;
     }
 };
