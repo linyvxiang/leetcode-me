2a3a64089ccd735af1427eb9eccebcfc5660de8e
yvxiang <linyvxiang@gmail.com>
2014-08-18 09:37:06 +0800

    rewrite

--- ValidPalindrome.cpp
+++ ValidPalindrome.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        string format;
-        if(!s.size()) {
-            return true;
-        }
-        int i,j=0;
-        for(i = 0;i < s.size(); i++) {
-            if(s[i]>='a' && s[i]<='z') {
-                format += s[i];
-            } else if(s[i]>='A' && s[i] <= 'Z') {
-                format += s[i] - 'A' + 'a';
-            } else if(s[i] >= '0' && s[i] <= '9') {
-                format += s[i];
-            } 
-        }
-        if(!format.size())
-            return true;
-        int begin=0,end=format.size()-1;
-        while(begin < end) {
-            if(format[begin] == format[end]) {
-                begin++;
+        int start = 0, end = s.size() - 1;
+        while(start <= end) {
+            while(start < s.size() && !is_alapha(s, start))
+                start++;
+            if(start >= end)
+                return true;
+            while(end >= 0 && !is_alapha(s, end))
                 end--;
-            } else {
+            if(end <= start)
+                return true;
+            if(!check(s, start, end))
                 return false;
-            }
+            start++;
+            end--;
         }
         return true;
+    }
+private:
+    bool is_alapha(string &s, int pos)
+    {
+        return 'a' <= s[pos] && s[pos] <= 'z' ||
+                'A' <= s[pos] && s[pos] <= 'Z' ||
+                '0' <= s[pos] && s[pos] <= '9';
+    }
+    bool check(string &s, int pos_a, int pos_b)
+    {
+        char ch_a = s[pos_a], ch_b = s[pos_b];
+        if('a' <= s[pos_a] && s[pos_a] <= 'z')
+            ch_a = ch_a - 'a' + 'A';
+        if('a' <= s[pos_b] && s[pos_b] <= 'z')
+            ch_b = ch_b - 'a' + 'A';
+        return ch_a == ch_b;
     }
 };
