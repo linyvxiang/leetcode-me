4dc505f51bf870a13699823746955da824d1d34f
yvxiang <linyvxiang@gmail.com>
2014-08-18 11:22:22 +0800

    rewrite

--- CombinationSum.cpp
+++ CombinationSum.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    void do_slove(vector<vector<int>> &res, vector<int> &cur_res, vector<int> num, int cur_sum, int cur_pos, int target)
-    {
-        if(cur_sum == target) {
-            res.push_back(cur_res);
-            return ;
-        }
-        if(cur_sum > target)
-            return ;
-        int i;
-        for(i = cur_pos; i < num.size(); i++) {
-            cur_res.push_back(num[i]);
-            do_slove(res, cur_res, num, cur_sum + num[i], i, target);
-            cur_res.pop_back();
-        }
-    }
-    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> cur_res;
-        vector<int> new_candidates;
-        int prev = -1, i;
-        for(i = 0; i < candidates.size(); i++) {
-            if(prev == candidates[i])
-                continue;
-            new_candidates.push_back(candidates[i]);
-            prev = candidates[i];
-        }
-        sort(new_candidates.begin(), new_candidates.end());
-        do_slove(res, cur_res, new_candidates, 0, 0, target);
+    vector<vector<int> > combinationSum(vector<int> &num, int target) {
+        vector<vector<int> > res;
+        vector<int> tmp_res;
+        if(num.empty())
+            return res;
+        do_generate_combination(num, 0, 0, target, res, tmp_res);
         return res;
+    }
+private:
+    void do_generate_combination(vector<int> &num, int cur_pos, int cur_sum, int target,
+                                    vector<vector<int> > &res, vector<int> &tmp_res)
+    {
+        if(cur_sum > target || cur_pos > num.size())
+            return ;
+        if(cur_sum == target) {
+            res.push_back(tmp_res);
+            return ;
+        }
+        int start;
+        for(start = cur_pos; start < num.size(); start++) {
+            if(start > cur_pos && num[start] == num[start - 1])
+                continue;
+            tmp_res.push_back(num[start]);
+            do_generate_combination(num, start, cur_sum + num[start],
+                                        target, res, tmp_res);
+            tmp_res.pop_back();
+        }
     }
 };
