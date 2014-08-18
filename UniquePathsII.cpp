9ed1f51b3b75d2288f88eb14e3c71b7c1e3c04ca
yvxiang <linyvxiang@gmail.com>
2014-08-18 15:21:59 +0800

    rewrite

--- UniquePathsII.cpp
+++ UniquePathsII.cpp
@@ -1,41 +1,23 @@
-Unique paths II
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
-        int col_num = obstacleGrid[0].size(), row_num = obstacleGrid.size();
+        int row = obstacleGrid.size(), column = obstacleGrid[0].size();
+        vector<vector<int> > paths(row, vector<int>(column, 0));
         int i, j;
-        int res[row_num][col_num];
-        bool flag  = false;
-        for(i = 0; i < col_num; i++) {
-            if(!flag && obstacleGrid[0][i] == 0)
-                res[0][i] = 1;
-            else if(!flag && obstacleGrid[0][i] == 1) {
-                flag = true;
-                res[0][i] = 0;
-            }
-            else
-                res[0][i] = 0;
-        }
-        flag = false;
-        for(i = 0; i < row_num; i++) {
-            if(!flag && obstacleGrid[i][0] == 0)
-                res[i][0] = 1;
-            else if(!flag && obstacleGrid[i][0] == 1) {
-                flag = true;
-                res[i][0] = 0;
-            }
-            else
-                res[i][0] = 0;
-        }// init finished
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[row_num - 1][col_num - 1] == 1)
-            return 0;
-        for(i = 1; i < row_num; i++)
-            for(j = 1; j < col_num; j++) {
+        paths[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+
+        for(i = 1; i < column; i++)
+            paths[0][i] = obstacleGrid[0][i] == 1 ? 0 : paths[0][i - 1];
+        for(j = 1; j < row; j++)
+            paths[j][0] = obstacleGrid[j][0] == 1 ? 0 : paths[j - 1][0];
+
+        for(i = 1; i < row; i++)
+            for(j = 1; j < column; j++) {
                 if(obstacleGrid[i][j] == 1)
-                    res[i][j] = 0;
+                    paths[i][j] = 0;
                 else
-                    res[i][j] = res[i - 1][j] + res[i][j - 1];
+                    paths[i][j] = paths[i][j - 1] + paths[i - 1][j];
             }
-        return res[row_num - 1][col_num - 1];
+        return paths[row - 1][column - 1];
     }
 };
