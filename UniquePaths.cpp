08ca2388ba6af0376d1c87df579c889bab68fc39
yvxiang <linyvxiang@gmail.com>
2014-08-18 11:37:05 +0800

    easy task

--- UniquePaths.cpp
+++ UniquePaths.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int res[m][n];
+        vector<vector<int> > paths(m, vector<int>(n, 0));
         int i, j;
-        for(i = 0; i < n; i++)
-            res[0][i] = 1;
-        for(i = 0; j < m; i++)
-            res[i][0] = 1;
+        paths[0][0] = 1;
+        for(i = 1; i < m; i++)
+            paths[i][0] = paths[i - 1][0];
+        for(j = 1; j < n; j++)
+            paths[0][j] = paths[0][j - 1];
         for(i = 1; i < m; i++)
             for(j = 1; j < n; j++)
-                res[i][j] = res[i - 1][j] + res[i][j - 1];
-        return res[m - 1][n - 1];
+                paths[i][j] = paths[i][j - 1] + paths[i - 1][j];
+        return paths[m - 1][n - 1];
     }
 };
