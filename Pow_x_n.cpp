f89ebc1f2ad8a2817ccc5939ce46df49948bb7a1
yvxiang <linyvxiang@gmail.com>
2014-08-18 18:21:26 +0800

    注意int型表示范围 注意当指数为负数时，对指数求反并不一定有效，这可能走出int型的表示范围，应该先除2

--- Pow_x_n.cpp
+++ Pow_x_n.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     double pow(double x, int n) {
-        if(n < 0)
-            return 1.0 / power(x, -n);
-        return power(x, n);
-    }
-private:
-    double power(double x, int n)
-    {
-        if(n == 0)
+        if(n == 0 || x == 1)
             return 1;
-        double mid = power(x, n / 2);
-        if(n % 2 == 0)
-            return mid * mid;
-        return mid * mid * x;
+        if(x == -1)
+            return n % 2 == 0 ? 1 : -1;
+        if(n < 0)
+            return 1.0 / pow(x, -n);
+        double mid = pow(x, n >> 1);
+        if(n & 0x1)
+            return mid * mid * x;
+        return mid * mid;
     }
 };
