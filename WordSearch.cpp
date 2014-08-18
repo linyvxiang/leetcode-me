b25875cf76f2b63b1a7ac6c810a820ccebfbbcc8
yvxiang <linyvxiang@gmail.com>
2014-08-18 10:31:26 +0800

    rewrite

--- WordSearch.cpp
+++ WordSearch.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
-	bool exist(vector<vector<char> > &board, string word) {
-		const int m = board.size();
-		const int n = board[0].size();
-		vector<vector<bool> > visited(m, vector<bool>(n, false));
-		for (int i = 0; i < m; ++i)
-			for (int j = 0; j < n; ++j)
-				if (dfs(board, word, 0, i, j, visited))
-					return true;
-				return false;
-	}
+    bool exist(vector<vector<char> > &board, string word) {
+        if(word.size() == 0)
+            return true;
+        if(board.empty())
+            return false;
+        int m = board.size(), n = board[0].size();
+        vector<vector<bool> > flag(m, vector<bool>(n, false));
+        int x, y;
+        for(x = 0; x < board.size(); x++)
+            for(y = 0; y < board[x].size(); y++)
+                if(do_judge(board, word, x, y, 0, flag))
+                    return true;
+           
+        return false;
+    }
 private:
-	static bool dfs(const vector<vector<char> > &board, const string &word,
-		int index, int x, int y, vector<vector<bool> > &visited) {
-		if (index == word.size())
-			return true; // 收敛条件
-		if (x < 0 || y < 0 || x >= board.size() || y >= board[0].size())
-			return false; // 越界，终止条件
-		if (visited[x][y]) return false; // 已经访问过，剪枝
-		if (board[x][y] != word[index]) return false; // 不相等，剪枝
-		visited[x][y] = true;
-		bool ret = dfs(board, word, index + 1, x - 1, y, visited) || // 上
-			dfs(board, word, index + 1, x + 1, y, visited) || // 下
-			dfs(board, word, index + 1, x, y - 1, visited) || // 左
-			dfs(board, word, index + 1, x, y + 1, visited); // 右
-		visited[x][y] = false;
-		return ret;
-	}
-}
+    bool do_judge(vector<vector<char> > &board, string &str,
+                    int x, int y, int cur_pos, vector<vector<bool> > &flag)
+    {
+        if(cur_pos == str.size())
+            return true;
+        if(x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || flag[x][y])
+            return false;
+        if(str[cur_pos] != board[x][y])
+            return false;
+        flag[x][y] = true;
+        bool ret = do_judge(board, str, x, y - 1, cur_pos + 1, flag) ||
+                    do_judge(board, str, x - 1, y, cur_pos + 1, flag) ||
+                     do_judge(board, str, x, y + 1, cur_pos + 1, flag) ||
+                      do_judge(board, str, x + 1, y, cur_pos + 1, flag);
+        flag[x][y] = false;
+        return ret;
+    }
+};
