8b2a7cab1adc909b85f1c8b5d085332d4c972a1a
yvxiang <linyvxiang@gmail.com>
2014-08-19 11:48:00 +0800

    rewrite

--- LetterCombinationsofaPhoneNumber.cpp
+++ LetterCombinationsofaPhoneNumber.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    vector<string> keyboard { " ", "", "abc", "def",
-                                "ghi", "jkl", "mno", "qprs", "tuv", "wxyz"};                                   
+    vector<string> dic{" ", " ", "abc", "def", "ghi", "jkl",
+                        "mno", "pqrs", "tuv", "wxyz"};
     vector<string> letterCombinations(string digits) {
         vector<string> res;
-        generate(digits, 0, "", res);       
+        do_generate_combinations(0, "", digits, res);
         return res;
     }
 private:
-    void generate(string &digits, int cur_pos, string tmp, vector<string> &res)
+    void do_generate_combinations(int cur_pos, string cur_str,
+                                    string &ori_str, vector<string> &res)
     {
-        if(cur_pos == digits.size()) {
-            res.push_back(tmp);
+        if(cur_pos == ori_str.size()) {
+            res.push_back(cur_str);
             return ;
         }
-        for(auto c : keyboard[digits[cur_pos] - '0'])
-            generate(digits, cur_pos + 1, tmp + c, res);
+        int i;
+        for(i = 0; i < dic[ori_str[cur_pos] - '0'].size(); i++)
+            do_generate_combinations(cur_pos + 1, cur_str + dic[ori_str[cur_pos] - '0'][i],
+                                        ori_str, res);
     }
 };
