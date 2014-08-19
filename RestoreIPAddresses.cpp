e3e29fe9c900c80c14fad76b6ca7d021c3ba38d3
yvxiang <linyvxiang@gmail.com>
2014-08-19 11:17:06 +0800

    rewrite

--- RestoreIPAddresses.cpp
+++ RestoreIPAddresses.cpp
@@ -2,36 +2,33 @@ class Solution {
 public:
     vector<string> restoreIpAddresses(string s) {
         vector<string> res;
-        if(s.size() == 0)
-            return res;
-        do_generate_ip_address(0, 0, s, "", res);
+        do_generate(0, 0, s, "", res);
         return res;
     }
 private:
-    void do_generate_ip_address(int cur_steps, int cur_pos, string target_str,
-                                        string cur_str, vector<string> &res)
+    void do_generate(int cur_pos, int cur_part_num, string &ori_str,
+                        string cur_str, vector<string> &res)
     {
-        if(cur_steps == 4 && cur_pos == target_str.size()) {
-            cur_str.resize(cur_str.size() - 1);
-            res.push_back(cur_str);
+        if(cur_pos == ori_str.size()) {
+            if(cur_part_num == 4) {
+                cur_str.resize(cur_str.size() - 1);
+                res.push_back(cur_str);
+            }
             return ;
         }
-        
-        if(target_str.size() - cur_pos < 4 - cur_steps)
+        if((4 - cur_part_num) * 3 < ori_str.size() - cur_str.size() + cur_part_num)
             return ;
-        if(target_str.size() - cur_pos > 3 * (4 - cur_steps))
+        if((4 - cur_part_num) > ori_str.size() - cur_str.size() + cur_part_num)
             return ;
-
         int num = 0, i;
         for(i = cur_pos; i < cur_pos + 3; i++) {
-            if(i >= target_str.size())
+            if(i >= ori_str.size())
                 break;
-            cur_str += target_str[i];
-            num = num * 10 + target_str[i] - '0';
+            num = num * 10 + ori_str[i] - '0';
+            cur_str += ori_str[i];
             if(num <= 255)
-                do_generate_ip_address(cur_steps + 1, i + 1, target_str,
-                                                    cur_str + '.', res);
-
+                do_generate(i + 1, cur_part_num + 1, ori_str,
+                            cur_str + '.', res);
             if(num == 0)
                 break;
         }
