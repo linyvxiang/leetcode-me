d4013e9b2f80ab44c2f1839b40baae584eb8fe91
yvxiang <linyvxiang@gmail.com>
2014-08-19 14:21:52 +0800

    rewrite

--- MinimumPathSum.cpp
+++ MinimumPathSum.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int> > &grid) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        int m = grid.size();
-        int n = grid[0].size();
-        if(m == 0 || n == 0)
-            return 0;
+        int row = grid.size(), column = grid[0].size();
         
-        int i, j, path_sum[m][n];
-        path_sum[0][0] = grid[0][0];
-    	for(i = 1; i < m; ++i)
-	    	path_sum[i][0] = path_sum[i - 1][0] + grid[i][0];
-    	for(j = 1; j < n; ++j)
-	    	path_sum[0][j] = path_sum[0][j - 1] + grid[0][j];
-            
-	    for(i = 1; i < m; ++i)
-	    	for(j= 1; j < n; ++j)
-		    	path_sum[i][j] = min(path_sum[i - 1][j], path_sum[i][j - 1])+ grid[i][j];
-        return path_sum[m - 1][n - 1];
-            
+        int i, j;
+        for(i = 1; i < column; i++)
+            grid[0][i] += grid[0][i - 1];
+        for(i = 1; i < row; i++)
+            grid[i][0] += grid[i - 1][0];
+        for(i = 1; i < row; i++)
+            for(j = 1; j < column; j++)
+                grid[i][j] += min(grid[i][j - 1], grid[i - 1][j]);
+        return grid[row - 1][column - 1];
     }
 };
