9656ea19670fd4e717c72f461fc62d9f0aed057b
yvxiang <linyvxiang@gmail.com>
2014-08-20 14:50:41 +0800

    rewrite

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        int pre = 0, now = 0;
         if(x < 0)
             return false;
-        while(pow(10, now) <= x) {
-            pre = now;
-            now++;
+        int n_x = x;
+        int r_x = 0;
+        while(x) {
+            r_x = r_x * 10 + x % 10;
+            x /= 10;
         }
-        int cur = 0;
-        while( cur <= pre / 2) {
-            if((x / (int)pow(10, cur)) % 10 != (x / (int)pow(10, pre - cur)) % 10)
-                return false;
-            cur++;
-        }
-        return true;
+        return r_x == n_x;
     }
 };
