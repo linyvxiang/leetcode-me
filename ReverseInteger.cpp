027e2accceb70c0f664eba6d950b50e9957de63b
yvxiang <linyvxiang@gmail.com>
2014-08-20 14:36:31 +0800

    rewrite

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int reverse(int x) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(!x)
-            return 0;
-        int ans = 0;
+        int res = 0;
         while(x) {
-            ans = ans * 10 + x % 10;
+            res = res * 10 + x  % 10;
             x /= 10;
         }
-        return ans;
+        return res;
     }
 };
