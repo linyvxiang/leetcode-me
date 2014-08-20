8313f8b439580228ae65f5221d8adae9b25014ae
yvxiang <linyvxiang@gmail.com>
2014-08-20 10:57:02 +0800

    贪心法 求出丙日之间的差值，将所有连续的正值加起来即可。 最后多添加一个-1，以方便将前面所有结果都算完

--- BestTimetoBuyandSellStockII.cpp
+++ BestTimetoBuyandSellStockII.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        int i, result = 0;
-        vector<int> diff;
-        vector<int> res;
-        diff.push_back(0);
-        res.push_back(0);
+        if(prices.empty())
+            return 0;
+        vector<int> diff(prices.size() - 1, 0);
+        int i;
         for(i = 1; i < prices.size(); i++)
-            diff.push_back(prices[i] - prices[i - 1]);
-        for(i = 1; i < diff.size(); i++) {
-            if(res[res.size() - 1] >= 0) {
-                if(diff[i] >= 0)
-                    result += diff[i];
-                res.push_back(res[res.size() - 1] + diff[i]);
+            diff[i- 1] = prices[i] - prices[i - 1];
+        diff.push_back(-1);
+        int cur_res = 0, sum = 0;
+        for(i = 0; i < diff.size(); i++) {
+            if(diff[i] >= 0) {
+                cur_res += diff[i];
             } else {
-                res.push_back(diff[i]);
-                if(diff[i] >= 0)
-                    result += diff[i];
+                sum += cur_res;
+                cur_res = 0;
             }
         }
-        return result;
+        return sum;
     }
 };
