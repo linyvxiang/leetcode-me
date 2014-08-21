0b6257bc3d6483cd153197ff6de67279aebbfeb4
yvxiang <linyvxiang@gmail.com>
2014-08-21 11:31:58 +0800

    rewrite

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int len1 = word1.size(), len2 = word2.size();
-        int res[len1 + 1][len2 + 1];
+        if(word1.size() == 0)
+            return word2.size();
+        if(word2.size() == 0)
+            return word1.size();
+        int paths[word1.size() + 1][word2.size() + 1];
         int i, j;
-        for(i = 0; i <= len1; i++)
-            res[i][0] = i;
-        for(j = 0; j <= len2; j++)
-            res[0][j] = j;
-        for(i = 1; i <= len1; i++)
-            for(j = 1; j<= len2; j++) {
-                if(word1[i - 1] == word2[j - 1])
-                    res[i][j] = res[i - 1][j - 1];
-                else {
-                    res[i][j] = min(res[i - 1][j] + 1, res[i][j - 1] + 1,
-                                    res[i - 1][j - 1] + 1);
+        for(i = 0; i <= word2.size(); i++)
+            paths[0][i] = i;
+        for(i = 0; i <= word1.size(); i++)
+            paths[i][0] = i;
+        for(i = 1; i <= word1.size(); i++) {
+            for(j = 1; j <= word2.size(); j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                      paths[i][j] = paths[i - 1][j - 1];
+                } else {
+                    paths[i][j] = min(paths[i - 1][j - 1],
+                                    min(paths[i][j - 1], paths[i - 1][j])) + 1;
                 }
             }
-        return res[len1][len2];
+        }
+        return paths[word1.size()][word2.size()];
     }
-private:
-    int min(int a, int b, int c)
-    {
-        int res = a;
-        int ano = b < c ? b : c;
-        return res < ano ? res : ano;
-    }
-      
 };
