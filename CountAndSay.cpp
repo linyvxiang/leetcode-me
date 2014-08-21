1d5975b7d799b22f76639202dcefec66b31bf271
yvxiang <linyvxiang@gmail.com>
2014-08-21 15:12:18 +0800

    rewrite

--- CountAndSay.cpp
+++ CountAndSay.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     string countAndSay(int n) {
+        if(n < 1)
+            return "";
         string res = "1";
-        n--;
-        while(n--) {
-            int fast = 0 , slow = 0;
-            string temp;
-            while(fast < res.size()) {
-                while(fast < res.size() && res[fast] == res[slow])
-                    fast++;
-                temp.insert(temp.end(), fast - slow + '0');
-                temp.insert(temp.end(), res[slow]);
-                slow = fast;
-                if(fast >= res.size())
-                  break;
-            }
-            res = temp;
-        }
+        int i;
+        for(i = 2; i <= n; i++)
+            res = do_generate(res);
         return res;
+    }
+private:
+    string do_generate(string &res)
+    {
+        int slow = 0, fast;
+        string tmp = "";
+        while(slow < res.size()) {
+            fast = slow + 1;
+            while(fast < res.size() && res[fast] == res[slow])
+                fast++;
+            tmp += fast - slow + '0';
+            tmp += res[slow];
+            slow = fast;
+        }
+        return tmp;
     }
 };
