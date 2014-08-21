b4515e784cf6176945afe12421262365cdbc265c
yvxiang <linyvxiang@gmail.com>
2014-08-21 15:54:01 +0800

    hehe

--- StringToInteger.cpp
+++ StringToInteger.cpp
@@ -3,8 +3,8 @@ public:
     int atoi(const char *str) {
         if(!str)
             return 0;
-        int s = 1;
         long long res = 0;
+        int s = 1;
         const char *p = str;
         while(p && *p == ' ')
             p++;
@@ -16,16 +16,18 @@ public:
         } else if(*p == '+') {
             p++;
             s = 1;
-        }
+        } 
         while(*p) {
-            if(!('0' <= *p && *p <= '9'))
+            if(!(*p >= '0' && *p <= '9'))
                 break;
-            if(res > INT_MAX / 10 || (res == INT_MAX / 10 &&
-                                *p - '0' > INT_MAX % 10))
-                return s == 1 ? INT_MAX : INT_MIN;
             res = res * 10 + *p - '0';
             p++;
         }
-        return res * s;
+        res *= s;
+        if(res > INT_MAX)
+            res = INT_MAX;
+        if(res < INT_MIN)
+            res = INT_MIN;
+        return (int)res;
     }
 };
