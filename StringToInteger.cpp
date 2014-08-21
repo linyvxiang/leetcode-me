fbeb1c85a08abd55ed010b3ef01a23727602c88d
yvxiang <linyvxiang@gmail.com>
2014-08-21 15:45:37 +0800

    hehe

--- StringToInteger.cpp
+++ StringToInteger.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     int atoi(const char *str) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-      long long ans = 0;
-      int flag = 1;
-      int len = strlen(str);
-      if(!str || strlen(str) == 0)
-        return 0;
-      int i = 0;
-      while(*str == ' ')
-        str++;
-      if(*str == '-') {
-          flag = -1;
-          str++;
-      } else if(*str == '+') {
-          flag = 1;
-          str++;
-      }
-      for(; *str != '\0'; str++) {
-          if(*str >= '0' && *str <= '9') {
-              ans = ans *10 + (*str) - '0';
-          } else {
-              break;
-          }
-      }
-      ans *= flag;
-      if(ans > INT_MAX)
-        ans = INT_MAX;
-      else if(ans < INT_MIN)
-        ans = INT_MIN;
-      return (int)ans;
+        if(!str)
+            return 0;
+        int s = 1;
+        long long res = 0;
+        const char *p = str;
+        while(p && *p == ' ')
+            p++;
+        if(!p)
+            return 0;
+        if(*p == '-') {
+            p++;
+            s = -1;
+        } else if(*p == '+') {
+            p++;
+            s = 1;
+        }
+        while(*p) {
+            if(!('0' <= *p && *p <= '9'))
+                break;
+            if(res > INT_MAX / 10 || (res == INT_MAX / 10 &&
+                                *p - '0' > INT_MAX % 10))
+                return s == 1 ? INT_MAX : INT_MIN;
+            res = res * 10 + *p - '0';
+            p++;
+        }
+        return res * s;
     }
 };
