c9f6e65899cb4d5023c0bf7b5109625b47aff859
yvxiang <linyvxiang@gmail.com>
2014-08-26 15:49:30 +0800

    空间复杂度O(n)的方法，所谓滚动数组。越写越像背包

--- DistinceSubsequences.cpp
+++ DistinceSubsequences.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     int numDistinct(string S, string T) {
         int row = S.size(), column = T.size();
-        int res[row + 1][column + 1];
-        memset(res, 0, sizeof(res));
+        vector<int> res(column + 1, 0);
+        res[0] = 1;
         int i, j;
-        res[0][0] = 1;
         for(i = 1; i <= row; i++)
-            res[i][0] = 1;
-        for(i = 1; i <= row; i++) {
-            for(j = 1; j <= column; j++) {
-                if(S[i - 1] != T[j - 1])
-                    res[i][j] = res[i - 1][j];
-                else
-                    res[i][j] = res[i - 1][j] + res[i - 1][j - 1];
-            }
-        }
-        return res[row][column];
+            for(j = column; j >= 1; j--)
+                if(S[i - 1] == T[j - 1])
+                    res[j] = res[j - 1] + res[j];
+        return res[column];
     }
 };
