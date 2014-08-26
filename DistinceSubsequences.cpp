5a9ce3b1d912b5eb913e7928227f93cbbef01690
yvxiang <linyvxiang@gmail.com>
2014-08-26 15:30:58 +0800

    DP版 设res[i][j]表示以S串的前i个字符，T串的前j个字符而得到的结果，那么可以得出如下转移方程： 1)若S[i - 1] != T[j - 1]，那么res[i][j] = res[i - 1][j] 2)若S[i - 1] == T[j - 1], 那么可以根据是不是使用S[i - 1]，来将情况再分为两类a)，不使用S[i - 1]，则这时res[i][j] = res[i - 1][j] b), 使用S[i - 1]， 则这时res[i][j] = res[i - 1][j - 1] 另外注意初始化S[1][0] = 1 写起来好像背包,以后这种二维的DP要注意了，基本上就是先解决以i, j为结尾的子问题，然后再根据i, j的状态进行判断

--- DistinceSubsequences.cpp
+++ DistinceSubsequences.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int numDistinct(string S, string T) {
-        return calc_substr_count(0, 0, 0, S, T);
-    }
-private:
-    int calc_substr_count(int cur_count, int cur_pos_s, int cur_pos_t, 
-                            string &S, string &T)
-    {
-        if(cur_pos_t == T.size())
-                return 1;
-        if(cur_pos_s >= S.size())
-            return 0;
-        if(S.size() - cur_pos_s  < T.size() - cur_pos_t)
-            return 0;
-        int s = cur_pos_s;
-        for(; s < S.size(); s++) {
-            if(S[s] == T[cur_pos_t])
-                return calc_substr_count(cur_count, s + 1, cur_pos_t + 1,
-                                            S, T) + 
-                        calc_substr_count(cur_count, s + 1, cur_pos_t, S, T);
-            else
-                return calc_substr_count(cur_count, s + 1, cur_pos_t, S, T);
+        int row = S.size(), column = T.size();
+        int res[row + 1][column + 1];
+        memset(res, 0, sizeof(res));
+        int i, j;
+        res[0][0] = 1;
+        for(i = 1; i <= row; i++)
+            res[i][0] = 1;
+        for(i = 1; i <= row; i++) {
+            for(j = 1; j <= column; j++) {
+                if(S[i - 1] != T[j - 1])
+                    res[i][j] = res[i - 1][j];
+                else
+                    res[i][j] = res[i - 1][j] + res[i - 1][j - 1];
+            }
         }
+        return res[row][column];
     }
 };
