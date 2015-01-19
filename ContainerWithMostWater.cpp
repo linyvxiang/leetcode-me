6c12bd94cb1a6349f37952a8dbe016e8f0ec4269
yvxiang <linyvxiang@gmail.com>
2015-01-19 18:42:49 +0800

    easy task

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int maxArea(vector<int> &height) {
+            int start = 0, end = height.size() - 1;
+            int max_area = 0;
+            while(start < end) {
+                int cur_area = (end - start) * min(height[start], height[end]);
+                max_area = max(max_area, cur_area);
+                if(height[start] < height[end])
+                    start++;
+                else
+                    end--;
+            }
+            return max_area;
+        }
+};
