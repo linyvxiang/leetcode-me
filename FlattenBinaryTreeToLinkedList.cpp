4d0b38925aa8e4c8a7b05ecf5d5f3a9c3d53b868
yvxiang <linyvxiang@gmail.com>
2015-05-10 11:43:52 +0800

    easy task

---
+++ FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* prev = NULL;
+        do_flatten(root, &prev);
+    }
+private:
+    void do_flatten(TreeNode* root, TreeNode** prev) {
+        if (!root) {
+            return;
+        }
+        if (*prev) {
+            (*prev)->left = NULL;
+            (*prev)->right = root;
+        }
+        *prev = root;
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        do_flatten(left, prev);
+        do_flatten(right, prev);
+    }
+};
