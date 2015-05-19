62267f5a9a422931f3992308a9b58c31215a8a25
yvxiang <linyvxiang@gmail.com>
2015-05-19 10:35:12 +0800

    better binary search

--- SearchInRotatedSortedArray.cpp
+++ SearchInRotatedSortedArray.cpp
@@ -8,33 +8,18 @@ private:
         if (start > end) {
             return -1;
         }
-        if (start == end || start == end - 1) {
-            if (target == nums[start]) {
-                return start;
-            } else if (target == nums[end]) {
-                return end;
-            } else {
-                return -1;
-            }
-        }
         int mid = (start + end) / 2;
         if (target == nums[mid]) {
             return mid;
         }
-        if (nums[mid] >= nums[start] && nums[mid] >= nums[end]) {
+        if (nums[start] < nums[mid]) {
             if (target >= nums[start] && target <= nums[mid]) {
-                return binary_search(nums, start, mid, target);
+                return binary_search(nums, start, mid - 1, target);
             } else {
-                return binary_search(nums, mid, end, target);
-            }
-        } else if (nums[mid] < nums[start] && nums[mid] < nums[end]) {
-            if (target >= nums[mid] && target <= nums[end]) {
-                return binary_search(nums, mid, end, target);
-            } else {
-                return binary_search(nums, start, mid, target);
+                return binary_search(nums, mid + 1, end, target);
             }
         } else {
-            if (target > nums[mid]) {
+            if (target >= nums[mid + 1] && target <= nums[end]) {
                 return binary_search(nums, mid + 1, end, target);
             } else {
                 return binary_search(nums, start, mid - 1, target);
