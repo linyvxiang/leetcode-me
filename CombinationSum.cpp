30f758d27f8c70211bb01e22f2d47ad330bc6b50
yvxiang <linyvxiang@gmail.com>
2015-05-24 12:25:34 +0800

    easy task

---
+++ CombinationSum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> cur_ret;
+        vector<vector<int> > ret;
+        if (candidates.empty()) {
+            return ret;
+        }
+        sort(candidates.begin(), candidates.end());
+        generate_combinations(candidates, 0, 0, target, cur_ret, ret);
+        return ret;
+    }
+private:
+    void generate_combinations(const vector<int>& nums,
+                               int cur_pos, int cur_sum,
+                               int target, vector<int>& cur_ret,
+                               vector<vector<int> >& ret) {
+        if (cur_sum == target) {
+            ret.push_back(cur_ret);
+        }
+        for (int pos = cur_pos; pos < nums.size(); pos++) {
+            if (pos != cur_pos && nums[pos] == nums[pos - 1]) {
+                continue;
+            }
+            if (cur_sum + nums[pos] > target) {
+                continue;
+            }
+            cur_ret.push_back(nums[pos]);
+            generate_combinations(nums, pos, cur_sum + nums[pos], target, cur_ret, ret);
+            cur_ret.pop_back();
+        }
+    }
+};
