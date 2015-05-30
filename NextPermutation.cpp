f39027ace08afabadf280ef21fb724d5aa7a3cb9
yvxiang <linyvxiang@gmail.com>
2015-05-30 14:49:31 +0800

    tricky

---
+++ NextPermutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int left_pos;
+        for (left_pos = nums.size() - 2; left_pos >= 0; left_pos--) {
+            if (nums[left_pos] <= nums[left_pos + 1]) {
+                break;
+            }
+        }
+        if (left_pos < 0) {
+            left_pos = 0;
+        }
+        int right_pos;
+        for (right_pos = nums.size() - 1; right_pos >= 0; right_pos--) {
+            if (nums[right_pos] >= nums[left_pos]) {
+                break;
+            }
+        }
+        swap(nums[left_pos], nums[right_pos]);
+        reverse(nums.begin() + right_pos, nums.end());
+    }
+};
