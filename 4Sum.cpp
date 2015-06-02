4154b388bfe4f012fc592901aaddc951dbb02585
yvxiang <linyvxiang@gmail.com>
2015-06-02 16:36:39 +0800

    another method

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -5,24 +5,25 @@ public:
         if (nums.size() < 4) {
             return ret;
         }
-        sort(ret.begin(), ret.end());
-        unordered_multimap<int, pair<int, int> > two_sum_map;
-        for (int i = 0; i + 1 < nums.size(); i++) {
+        sort(nums.begin(), nums.end());
+        unordered_map<int, vector<pair<int, int> > > two_sum_map;
+        for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
-                two_sum_map.insert(make_pair(nums[i] + nums[j], make_pair(i, j)));
+                two_sum_map[nums[i] + nums[j]].push_back(pair<int, int>(i, j));
             }
         }
-        for (auto i = two_sum_map.begin(); i != two_sum_map.end(); ++i) {
-            auto j = two_sum_map.equal_range(target - i->first);
-            for (auto it = j.first; it != j.second; ++it) {
-                int a = i->second.first;
-                int b = i->second.second;
-                int c = it->second.first;
-                int d = it->second.second;
-                if (a != c && a != d && b != c && b != d) {
-                    vector<int> cur_ret = { nums[a], nums[b], nums[c], nums[d] };
-                    sort(cur_ret.begin(), cur_ret.end());
-                    ret.push_back(cur_ret);
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                int key = target - nums[i] - nums[j];
+                if (two_sum_map.find(key) == two_sum_map.end()) {
+                    continue;
+                }
+                auto& vec = two_sum_map[key];
+                for (int k = 0; k < vec.size(); k++) {
+                    if (j >= vec[k].first) {
+                        continue;
+                    }
+                    ret.push_back( { nums[i], nums[j], nums[vec[k].first], nums[vec[k].second] } );
                 }
             }
         }
