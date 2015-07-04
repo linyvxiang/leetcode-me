bf8a51635e1beaad8f33de10531652e2a1b2db59
yvxiang <linyvxiang@gmail.com>
2015-07-04 19:20:59 +0800

    max heap

--- KthLargestElementInAnArray.cpp
+++ KthLargestElementInAnArray.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() - k];
+        make_heap(nums.begin(), nums.end());
+        for (int i = 1; i < k; i++) {
+            pop_heap(nums.begin(), nums.end());
+            nums.pop_back();
+        }
+        return nums[0];
     }
 };
-
