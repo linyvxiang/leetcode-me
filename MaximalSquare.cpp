ab6218e1f0e0600f6f6b9aebf04b4cf60abd53a6
yvxiang <linyvxiang@gmail.com>
2015-07-11 14:46:53 +0800

    DP

--- MaximalSquare.cpp
+++ MaximalSquare.cpp
@@ -1,51 +1,50 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int row = 0;
-        int col = 0;
-        int max_area = 0;
-        while (row < matrix.size()) {
-            col = 0;
-            while (col < matrix[0].size()) {
-                int right = col;
-                while (right < matrix[0].size() &&
-                        matrix[row][right] == '1') {
-                    right++;
-                }
-                int down = row;
-                while (down < matrix.size() &&
-//                        down - row <= right - col &&
-                        matrix[down][col] == '1') {
-                    down++;
-                }
-                int len = check_all_one(matrix, row, col, right, down);
-                if (len) {
-                    max_area = max(max_area, len * len);
-                    col++;
+        if (matrix.empty()) {
+            return 0;
+        }
+        vector<vector<int>> max_len(matrix.size(), vector<int>(matrix[0].size(), 0));
+        int len = 0;
+        for (int i = 0; i < matrix[0].size(); i++) {
+            if (matrix[0][i] == '1') {
+                max_len[0][i] = 1;
+            } else {
+                max_len[0][i] = 0;
+            }
+            len = max(len, max_len[0][i]);
+        }
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][0] == '1') {
+                max_len[i][0] = 1;
+            } else {
+                max_len[i][0] = 0;
+            }
+        }
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == '0') {
+                    max_len[i][j] = 0;
                 } else {
-                    col++;
-                }
-            }
-            row++;
-        }
-        return max_area;
-    }
-private:
-    int check_all_one(const vector<vector<char>>& matrix,
-            int row, int col, int right, int down) {
-        int len = min(down - row, right - col);
-        if (len == 0) {
-            return false;
-        }
-        for (int l = 0; l < len; l++) {
-            for (int i = 0; i <= l; i++) {
-                for (int j = 0; j <= l; j++) {
-                    if (matrix[row + i][col + j] == '0') {
-                        return l;
+                    /*
+                    if (matrix[i][j - 1] == '1' && matrix[i - 1][j] == '1') {
+                        max_len[i][j] = max_len[i - 1][j - 1] + 1;
+                    } else {
+                        max_len[i][j] = 1;
                     }
+                    above is wrong in the following case:
+                    0 0 0 1
+                    1 1 0 1
+                    1 1 1 1
+                    0 1 1 1
+                    0 1 1 1
+                    */
+                    max_len[i][j] = min(max_len[i - 1][j - 1],
+                            min(max_len[i][j - 1], max_len[i - 1][j])) + 1;
                 }
+                len = max(len, max_len[i][j]);
             }
         }
-        return len;
+        return len * len;
     }
 };
