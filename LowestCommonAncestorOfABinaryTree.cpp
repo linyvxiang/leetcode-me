5a2b254a205cce752161c83c9c7fd122a7871845
yvxiang <linyvxiang@gmail.com>
2015-07-16 17:31:50 +0800

    O(N) algorithm

--- LowestCommonAncestorOfABinaryTree.cpp
+++ LowestCommonAncestorOfABinaryTree.cpp
@@ -1,27 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) {
+            return NULL;
+        }
         if (root == p || root == q) {
             return root;
         }
-        bool p_left = find_node(root->left, p);
-        bool q_left = find_node(root->left, q);
-        if (p_left && q_left) {
-            return lowestCommonAncestor(root->left, p, q);
-        } else if (!p_left && !q_left) {
-            return lowestCommonAncestor(root->right, p, q);
-        } else {
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) {
             return root;
         }
-    }
-private:
-    bool find_node(TreeNode* root, TreeNode* target) {
-        if (!root) {
-            return false;
-        } else if (root == target) {
-            return true;
-        }
-        return find_node(root->left, target) ||
-                find_node(root->right, target);
+        return left ? left : right;
     }
 };
