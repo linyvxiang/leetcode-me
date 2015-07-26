8a8cb44deb8dcb78e88c424f5834f636bef669e5
yvxiang <linyvxiang@gmail.com>
2015-07-26 08:31:58 +0800

    easy task

---
+++ MinimumDepthOfBinaryTree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        } else if (!root->left && !root->right) {
+            return 1;
+        } else if (root->left && root->right) {
+            return 1 + min(minDepth(root->left), minDepth(root->right));
+        } else {
+            return 1 + (root->left ? minDepth(root->left) : minDepth(root->right));
+        }
+    }
+};
