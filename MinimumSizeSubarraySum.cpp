08bdc7cb26f81c836aeceb0e5e57d54122744912
yvxiang <linyvxiang@gmail.com>
2015-08-04 18:10:13 +0800

    hard to understand

--- MinimumSizeSubarraySum.cpp
+++ MinimumSizeSubarraySum.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
-        int cur_sum = 0;
-        int cur_start = 0;
-        int cur_end = 0;
+        vector<int> sum(nums.size() + 1, 0);
+        for (int i = 1; i < sum.size(); i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
         int min_len = INT_MAX;
-        while (cur_start < nums.size()) {
-            while (cur_end < nums.size() && cur_sum < s) {
-                cur_sum += nums[cur_end];
-                cur_end++;
+        for (int i = 0; i < sum.size(); i++) {
+            int lower_bound = find_lower_bound(sum, i + 1, sum.size() - 1, s + sum[i]);
+            if (lower_bound == sum.size()) {
+                break;
             }
-            if (cur_sum >= s) {
-                min_len = min(min_len, cur_end - cur_start);
-            }
-            cur_sum -= nums[cur_start];
-            cur_start++;
+            min_len = min(min_len, lower_bound - i);
         }
         return min_len == INT_MAX ? 0 : min_len;
+    }
+private:
+    int find_lower_bound(vector<int>& num, int start, int end, int target) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (num[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
     }
 };
