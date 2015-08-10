bbb8a9dd3f08d6da868802781562b51b84d31497
yvxiang <linyvxiang@gmail.com>
2015-08-10 10:51:26 +0800

    tricky

--- NumberOf1Bits.cpp
+++ NumberOf1Bits.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int cur_pos = 0;
-        int ret = 0;
-        while (cur_pos < 32) {
-            ret += n & 0x1;
-            n >>= 1;
-            cur_pos++;
+        int num = 0;
+        while (n) {
+            n &= (n - 1);
+            num++;
         }
-        return ret;
+        return num;
     }
 };
-
