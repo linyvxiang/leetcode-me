1c9443b58b04e944cb9193430307f1023d4da273
yvxiang <linyvxiang@gmail.com>
2015-08-13 20:04:11 +0800

    more clear

--- SearchForARange.cpp
+++ SearchForARange.cpp
@@ -1,45 +1,48 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lower_bound = find_lower_bound(nums, target);
-        int upper_bound = find_upper_bound(nums, target);
-        if (lower_bound <= upper_bound) {
-            return vector<int> {lower_bound, upper_bound};
-        } else {
-            return vector<int> {-1, -1};
+        vector<int> ret;
+        if (nums.empty()) {
+            ret.push_back(-1);
+            ret.push_back(-1);
+            return ret;
         }
+        int left = find_lower_bound(nums, target);
+        if (left >= nums.size() || nums[left] != target) {
+            ret.push_back(-1);
+            ret.push_back(-1);
+            return ret;
+        }
+        int right = find_upper_bound(nums, target);
+        ret.push_back(left);
+        ret.push_back(right);
+        return ret;
     }
 private:
-    int find_lower_bound(const vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] == target) {
-                end = mid - 1;
-            } else if (nums[mid] > target) {
-                end = mid - 1;
+    int find_lower_bound(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
             } else {
-                start = mid + 1;
+                left = mid + 1;
             }
         }
-        cout << "start = " << start << endl;
-        return start;
+        return left;
     }
-    int find_upper_bound(const vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] == target) {
-                start = mid + 1;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
+    int find_upper_bound(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
             } else {
-                end = mid - 1;
+                right = mid - 1;
             }
         }
-        cout << "end = " << end << endl;
-        return end;
+        return right;
     }
 };
