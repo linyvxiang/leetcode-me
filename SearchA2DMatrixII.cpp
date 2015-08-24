7aebf1e770f76c3bc4c3f4d68a52614a795e3657
yvxiang <linyvxiang@gmail.com>
2015-08-24 15:27:46 +0800

    more clear solution

--- SearchA2DMatrixII.cpp
+++ SearchA2DMatrixII.cpp
@@ -1,70 +1,19 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        return do_search(matrix, 0, 0, target);
+        return do_search(matrix, 0, matrix[0].size() - 1, target);
     }
 private:
-    bool find_in_array(vector<vector<int>>& matrix, int row, 
-                        int col, int target,  int direc) {
-        if (row >= matrix.size() || col >= matrix[0].size()) {
+    bool do_search(vector<vector<int>>& matrix, int x, int y, int target) {
+        if (x >= matrix.size() || y < 0) {
             return false;
         }
-        if (direc == 0) { //down 
-            int end = matrix.size() - 1;
-            int start = row;
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (matrix[mid][col] == target) {
-                    return true;
-                } else if (matrix[mid][col] > target) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-            return false;
-        } else { // right
-            int end = matrix[0].size() - 1;
-            int start = col;
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (matrix[row][mid] == target) {
-                    return true;
-                } else if (matrix[row][mid] > target) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-            return false;
-        }
-    }
-    bool do_search(vector<vector<int>>& matrix, int row, int col, int target) {
-        if (row >= matrix.size() || col >= matrix[0].size()) {
-            return false;
-        }
-        if (matrix[row][col] == target) {
+        if (matrix[x][y] == target) {
             return true;
-        }
-        if (col + 1 >= matrix[0].size()) {
-            return find_in_array(matrix, row, col, target, 0);// 0 stands for down
-        }
-        if (row + 1 >= matrix.size()) {
-            return find_in_array(matrix, row, col, target, 1); // 1 stands for right
-        }
-        if (matrix[row + 1][col] > target && matrix[row][col + 1] > target) {
-            return false;
-        } else if (matrix[row + 1][col] > target && matrix[row][col] < target) {
-            return find_in_array(matrix, row, col, target, 1);
-        } else if (matrix[row][col] < target && matrix[row][col + 1] > target) {
-            return find_in_array(matrix, row, col, target, 0);
+        } else if (matrix[x][y] < target) {
+            return do_search(matrix, x + 1, y, target);
         } else {
-            bool ret = find_in_array(matrix, row, col, target, 0);
-            if (ret) {
-                return ret;
-            } else {
-                return  do_search(matrix, row, col + 1, target);
-            }
+            return do_search(matrix, x, y - 1, target);
         }
     }
 };
